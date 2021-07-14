334994e3eb6cdef868b28e6dd79e624f7a1419a0
Osman <osharifi@ucdavis.edu>
2021-07-14 07:52:30 -0700

    Mecp2 details

--- custom_refgenome.py
+++ custom_refgenome.py
@@ -12,16 +12,16 @@ def wrap(seq, step=60):
 #Read in fasta file and change specific sequences into Ns
 for id, seq in korflib.read_fasta(sys.argv[1]):
 	if id.startswith("X"):
-		beg = 74026592
-		end = 74085597
+		beg = 74036400
+		end = 74085786		#exon beg is 74085691 
 		mut = 74085586
 		mx = seq[:beg] + "N"*(end-beg) + seq[end:]
 		print(f">{id}")
 		wrap(mx)
-		print(">Mecp2_e1")
+		print(">Mecp2_e1 dna:chromosome chromosome:GRCm38:Mecp2_e1:1:49386:1 REF")
 		wrap(seq[beg:end])
-		print(">Mecp2_e2")
-		wrap(seq[beg:mut] + "t" + seq[mut+1:end])
+		print(">Mecp2_e2 dna:chromosome chromosome:GRCm38:Mecp2_e2:1:49386:1 REF")
+		wrap(seq[beg:mut-1] + "a" + seq[mut:end])
 	else:
 		print(f">{id}")
 		wrap(seq)
