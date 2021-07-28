f280436d24a19aa318f540f08ed99a8d6d1df286
Viktoria Haghani <vhaghani@ucdavis.edu>
2021-07-28 15:56:54 -0700

    edit analyses for P60

--- scripts/02_DEG_analysis/DEG_EdgeR_analysis.R
+++ scripts/02_DEG_analysis/DEG_EdgeR_analysis.R
@@ -11,8 +11,9 @@ library(glue)
 
 # Paths
 
-data_file <- "~/GitHub/snRNA-seq-pipeline/raw_data/rett_E18_with_labels_proportions.rda"
+# data_file <- "~/GitHub/snRNA-seq-pipeline/raw_data/rett_E18_with_labels_proportions.rda"
 # data_file <- "~/GitHub/snRNA-seq-pipeline/raw_data/rett_P30_with_labels_proportions.rda"
+data_file <- "~/GitHub/snRNA-seq-pipeline/raw_data/rett_P60_with_labels_proportions.rda"
 
 DEG_data_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/EdgeR/"
 
