8eb23b7f3568a0aa1066a8fc1a9023817ce577e4
Osman <osharifi@ucdavis.edu>
2021-09-14 17:03:47 -0700

    Update velocyto.sh

--- velocyto.sh
+++ velocyto.sh
@@ -14,13 +14,4 @@ conda activate /share/lasallelab/Osman/RettSingleCellRNAseq/01-Cellranger/velocy
 #pip install velocyto
 
 #Run Velocyto
-velocyto run --bcfile
-/share/lasallelab/Osman/RettSingleCellRNAseq/01-Cellranger/19_0106_C/outs/
-raw_feature_bc_matrix/barcodes.tsv.gz --mask
-/share/workshop/adv_scrnaseq/$USER/references/GRCh38_rmsk.gtf --outputfolder
-/share/lasallelab/Osman/RettSingleCellRNAseq/01-Cellranger/02-Velocyto
---samtools-threads 42 --samtools-memory 10000
-/share/lasallelab/Osman/RettSingleCellRNAseq/01-Cellranger/19_0106_C/outs/
-possorted_genome_bam.bam
-/share/workshop/adv_scrnaseq/$USER/references/refdata-gex-GRCh38-2020-A/genes/
-genes.gtf > velocyto_sample1.err > velocyto_sample1.out
+velocyto run --bcfile /share/lasallelab/Osman/RettSingleCellRNAseq/01-Cellranger/19_0106_C/outs/raw_feature_bc_matrix/barcodes.tsv.gz --mask /share/workshop/adv_scrnaseq/$USER/references/GRCh38_rmsk.gtf --outputfolder /share/lasallelab/Osman/RettSingleCellRNAseq/01-Cellranger/02-Velocyto --samtools-threads 42 --samtools-memory 10000 /share/lasallelab/Osman/RettSingleCellRNAseq/01-Cellranger/19_0106_C/outs/possorted_genome_bam.bam /share/workshop/adv_scrnaseq/$USER/references/refdata-gex-GRCh38-2020-A/genes/genes.gtf > velocyto_sample1.err > velocyto_sample1.out
