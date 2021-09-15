7aecd6c10ccf807bdd4bd0c2c4931bfd5f0de110
Osman Sharifi <50931327+osmansharifi@users.noreply.github.com>
2021-09-15 14:25:19 -0700

    Update ComplexHeatmap_DEG.R

--- scripts/03_DEG_visualization/ComplexHeatmap_DEG.R
+++ scripts/03_DEG_visualization/ComplexHeatmap_DEG.R
@@ -1,3 +1,13 @@
+#Integrate all data frames together to create a DEG heatmap
+data.frame.list = list(L2_3_IT_DEG, L4_DEG, L5_DEG, L6_DEG, Sst_DEG, Pvalb_DEG, Vip_DEG, Oligo_DEG, Lamp5_DEG, Nonneuronal_DEG)
+#gene.set = unique(rownames(Reduce(rbind, data.frame.list)))
+gene.set = unique(unlist(lapply(data.frame.list, rownames)))
+ct.set   = unique(colnames(Reduce(cbind, data.frame.list)))
+DEG.matrix = matrix(0, nrow=length(gene.set), ncol=length(ct.set), dimnames=list(gene.set, ct.set))
+for(df in data.frame.list){
+  DEG.matrix[rownames(df), colnames(df)] = df[,1] ## Not sure what here, pvalue? foldchange? or binary existence
+}
+DEG.df = as.data.frame(DEG.matrix)
 library(ComplexHeatmap)
 ## Draws a heatmap with rows and columns seperated by a predefined label using ComplexHeatmap
 plotDEG = function(data,       ## Data matrix
