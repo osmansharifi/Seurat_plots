6f9337977b64ee0b9461dfc2a6cad376981d8703
Osman <osharifi@ucdavis.edu>
2021-11-02 13:15:06 -0700

    Update run_alignment.R

--- scripts/01_Data_preparation/run_alignment.R
+++ scripts/01_Data_preparation/run_alignment.R
@@ -2,19 +2,19 @@ library(scAlign)
 library(SingleCellExperiment)
 
 ## Load in mouse
-load("/share/lasallelab/Osman/scAlign_collab/processed/mouse.rda")
+load("/share/lasallelab/rett_female/scAlign_collab/processed/mouse.rda")
 
-## Load in Osman data
-load('/share/lasallelab/Osman/scAlign_collab/clusters_seurat_object.RData')
+## Load in rett_female data
+load('/share/lasallelab/rett_female/scAlign_collab/clusters_seurat_object.RData')
 rett_female = experiment.aggregate
 
 ## Common genes
 mouse <- FindVariableFeatures(mouse, do.plot = F, nFeature=3000)
-osman <- FindVariableFeatures(osman, do.plot = F, nFeature=3000)
+rett_female <- FindVariableFeatures(rett_female, do.plot = F, nFeature=3000)
 genes.use = Reduce(intersect, list(VariableFeatures(mouse),
-                                   VariableFeatures(osman),
+                                   VariableFeatures(rett_female),
                                    rownames(mouse),
-                                   rownames(osman)))
+                                   rownames(rett_female)))
 
 ## Create paired dataset SCE objects to pass into scAlignCreateObject
 mouseSCE <- SingleCellExperiment(
@@ -23,20 +23,21 @@ mouseSCE <- SingleCellExperiment(
                   scale.data = mouse@assays$RNA@scale.data[genes.use,])
 )
 
-osmanSCE <- SingleCellExperiment(
-  assays = list(counts = osman@assays$RNA@counts[genes.use,],
-                logcounts  = osman@assays$RNA@data[genes.use,],
-                scale.data = osman@assays$RNA@scale.data[genes.use,])
+rett_femaleSCE <- SingleCellExperiment(
+  assays = list(counts = rett_female@assays$RNA@counts[genes.use,],
+                logcounts  = rett_female@assays$RNA@data[genes.use,],
+                scale.data = rett_female@assays$RNA@scale.data[genes.use,])
 )
 
-scAlignRETT = scAlignCreateObject(sce.objects = list("mouse"=mouseSCE, "osman"=osmanSCE),
-                                 #labels =
+scAlignRETT = scAlignCreateObject(sce.objects = list("mouse"=mouseSCE, "rett_female"=rett_femaleSCE),
+                                 #labels = list(labels[which("mouse" = mouseSCE)],
+                                              # lables[which("rett_female" = rett_femaleSCE)]),
                                  data.use="scale.data",
                                  pca.reduce = TRUE,
                                  pcs.compute = 50,
                                  cca.reduce = TRUE,
                                  ccs.compute = 15,
-                                 project.name = "scAlign_Osman_Rett")
+                                 project.name = "scAlign_female_Rett")
 
 ## Run scAlign with high_var_genes as input to the encoder (alignment) and logcounts with the decoder (projections).
 scAlignRETT = scAlign(scAlignRETT,
