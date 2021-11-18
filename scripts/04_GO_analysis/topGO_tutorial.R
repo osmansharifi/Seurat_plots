6fe5b28727c184df3aadbea593acd40b62addcfd
Viktoria Haghani <vhaghani@ucdavis.edu>
2021-11-18 11:08:41 -0800

    comment out topgo object

--- scripts/04_GO_analysis/topGO_tutorial.R
+++ scripts/04_GO_analysis/topGO_tutorial.R
@@ -8,11 +8,11 @@ library(package = affyLib, character.only = TRUE)
 
 sum(topDiffGenes(geneList))
 
-sampleGOdata <- new("topGOdata",
-                    description = "Simple session", ontology = "BP",
-                    allGenes = geneList, geneSel = topDiffGenes,
-                    nodeSize = 10,
-                    annot = annFUN.db, affyLib = affyLib)
+#sampleGOdata <- new("topGOdata",
+#                    description = "Simple session", ontology = "BP",
+#                    allGenes = geneList, geneSel = topDiffGenes,
+#                    nodeSize = 10,
+#                    annot = annFUN.db, affyLib = affyLib)
 
 geneList
 typeof(geneList)
