0024916472323ae4b193d29fdc18d332d693cfaf
Viktoria Haghani <vhaghani@ucdavis.edu>
2021-11-18 10:46:06 -0800

    Create topGO_tutorial.R

---
+++ scripts/04_GO_analysis/topGO_tutorial.R
@@ -0,0 +1,15 @@
+library(topGO)
+library(ALL)
+data(ALL)
+data(geneList)
+
+affyLib <- paste(annotation(ALL), "db", sep = ".")
+library(package = affyLib, character.only = TRUE)
+
+sum(topDiffGenes(geneList))
+
+sampleGOdata <- new("topGOdata",
+                    + description = "Simple session", ontology = "BP",
+                    + allGenes = geneList, geneSel = topDiffGenes,
+                    + nodeSize = 10,
+                    + annot = annFUN.db, affyLib = affyLib)
