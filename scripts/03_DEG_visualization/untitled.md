7b76e70deda8405b639defbc5dec4c056fd0d152
Osman <osharifi@ucdavis.edu>
2021-12-02 15:33:50 -0800

    venn_diagram maker

---
+++ scripts/03_DEG_visualization/untitled.md
@@ -0,0 +1,6 @@
+set1 = set(['A', 'B', 'C', 'D'])
+set2 = set(['B', 'C', 'D', 'E'])
+set3 = set(['C', 'D',' E', 'F', 'G'])
+
+venn3([set1, set2, set3], ('Set1', 'Set2', 'Set3'))
+plt.show()
---
+++ scripts/03_DEG_visualization/venn_maker.py
@@ -0,0 +1,41 @@
+%matplotlib inline
+import matplotlib_venn as vplt
+from matplotlib import pyplot as plt
+from matplotlib_venn import venn2
+import pandas as pd
+
+#load complete data frame
+complete_df = pd.read_csv('/Users/osman/Documents/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/all_cort_all_dataframe.csv')
+print(complete_df)
+
+#select significant genes and parse out males from females
+complete_df = complete_df.sort_values(by='pv', ascending=True)
+sig_df = complete_df[complete_df['pv'] < 0.05]
+filtered = sig_df[sig_df['logfc'] >1]
+print(filtered)
+#print(len(sig_df))
+male_genes = sig_df[sig_df['sex'] == 'M']
+female_genes = sig_df[sig_df['sex'] == 'F']
+print('There are', len(male_genes),'number of male DEGs.')
+print('There are', len(female_genes),'number of female DEGs.')
+#print(male_genes, female_genes)
+
+print(len(male_genes), len(female_genes))
+deduped_male = male_genes.drop_duplicates(subset=['gene'])
+deduped_female = female_genes.drop_duplicates(subset=['gene'])
+#print(len(deduped_male), len(deduped_female))
+#print(deduped_male)
+print('There are', len(deduped_male),'number of male DEGs after deduplication.')
+print('There are', len(deduped_female),'number of female DEGs after deduplication.')
+
+#plot a venn diagram of the differences between female and male genes
+plt.figure(figsize=(12,12))
+set1 = set(deduped_male['gene'])
+set2 = set(deduped_female['gene'])
+venn2([set1, set2], ('male_DEGs', 'female_DEGs'), set_colors=('purple', 'skyblue'), alpha = 0.7)
+plt.title("Venn diagram of all DEGs from males and females")
+plt.show()
+
+#Extract genes that are the intersection
+print(len(set1.intersection(set2)))
+print(set1.intersection(set2))
