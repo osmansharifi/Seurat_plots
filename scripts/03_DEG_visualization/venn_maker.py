0ee0c4fa83fb64166f440f507192668117da4e45
Osman <osharifi@ucdavis.edu>
2021-12-06 17:20:36 -0800

    arg parse included in venn_maker

--- scripts/03_DEG_visualization/venn_maker.py
+++ scripts/03_DEG_visualization/venn_maker.py
@@ -1,17 +1,33 @@
-%matplotlib inline
 import matplotlib_venn as vplt
 from matplotlib import pyplot as plt
 from matplotlib_venn import venn2
 import pandas as pd
+import argparse
+
+# setup
+parser = argparse.ArgumentParser(
+	description='This will make a venn diagram of DEGs')
+# required arguments
+parser.add_argument('--csv', required=True, type=str,
+	metavar='<str>', help='path to CSV containing DEGs')
+parser.add_argument('--pdf', required=True, type=str,
+	metavar='<str>', help='path to pdf output file')
+parser.add_argument('--pv', required=False, type=float, default=0.05,
+	metavar='<float>', help='set the p-value cuttoff [%(default).2f]')
+parser.add_argument('--logfc', required=False, type=float, default=0.7,
+	metavar='<float>', help='set the logFC cutoff [%(default).1f]')
+# finalization
+arg = parser.parse_args()
 
 #load complete data frame
-complete_df = pd.read_csv('/Users/osman/Documents/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/all_cort_all_dataframe.csv')
+complete_df = pd.read_csv(arg.csv)
 print(complete_df)
+#'/Users/osman/Documents/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/all_cort_all_dataframe.csv
 
 #select significant genes and parse out males from females
 complete_df = complete_df.sort_values(by='pv', ascending=True)
-sig_df = complete_df[complete_df['pv'] < 0.05]
-sig_df = sig_df[sig_df['logfc'].abs() > 0.7]
+sig_df = complete_df[complete_df['pv'] < arg.pv]
+sig_df = sig_df[sig_df['logfc'].abs() > arg.logfc]
 #print(len(sig_df))
 male_genes = sig_df[sig_df['sex'] == 'M']
 female_genes = sig_df[sig_df['sex'] == 'F']
@@ -34,7 +50,8 @@ set1 = set(deduped_male['gene'])
 set2 = set(deduped_female['gene'])
 venn2([set1, set2], ('male_DEGs', 'female_DEGs'), set_colors=('purple', 'skyblue'), alpha = 0.7)
 plt.title("Venn diagram of all DEGs from males and females")
-plt.savefig('/Users/osman/Documents/GitHub/snRNA-seq-pipeline/figures/venn_diagrams/all_female_male_venn.pdf')  
+plt.savefig(arg.pdf)  
+#'/Users/osman/Documents/GitHub/snRNA-seq-pipeline/figures/venn_diagrams/all_female_male_venn.pdf'
 #plt.show()
 
 #Extract genes that are the intersection
@@ -53,4 +70,3 @@ with open('intersection_genes_test.csv', 'w') as fp:
     fp.write('intersection genes for males and females' + '\n')
     for gene in gene_list:
         fp.write(gene + '\n')
-            
