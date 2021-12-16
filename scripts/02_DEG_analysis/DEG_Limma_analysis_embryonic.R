dc5e4ff37073ac73d86956e4be7102bceb30951e
Viktoria Haghani <vhaghani@ucdavis.edu>
2021-12-16 14:24:10 -0800

    start working on female data

--- scripts/02_DEG_analysis/DEG_Limma_analysis_embryonic.R
+++ scripts/02_DEG_analysis/DEG_Limma_analysis_embryonic.R
@@ -13,7 +13,8 @@ library(glue)
 data_file <- "~/GitHub/snRNA-seq-pipeline/raw_data/all_female_E18.Rdata"
 
 # DEG_data_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/Limma/M_MUT_and_WT_M_E18_WB/"
-DEG_data_dir_total_genes <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/"
+DEG_data_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/Limma/M_MUT_and_WT_F_E18_WB/"
+# DEG_data_dir_total_genes <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/"
 
 # Lists
 cell_types <- list("L2_3_IT", "L6", "Sst", "L5", "L4", "Pvalb", "Sncg", "Non_neuronal", "Oligo", "Vip", "Lamp5", "Astro", "Peri", "Endo") 
@@ -37,7 +38,8 @@ experiment.aggregate <- RenameIdents(object = experiment.aggregate, 'Non-neurona
 ################################################################################
 ## Limma Analysis
 
-## To get only significant genes:
+# For MALES:
+# To get only significant genes:
 # for (cell_type in cell_types){
 #   cluster_cell <- subset(experiment.aggregate, idents = cell_type)
 #   expr_cell <- as.matrix(GetAssayData(cluster_cell))
@@ -61,6 +63,32 @@ experiment.aggregate <- RenameIdents(object = experiment.aggregate, 'Non-neurona
 #   # Write data to CSV so analysis does not need to be rerun when working with data
 #   write.csv(cell_Limma_DEG, file = glue(DEG_data_dir, cell_type, "_", metadata_info, "_Limma_DEG.csv"))
 # }
+
+# For FEMALES:
+# To get only significant genes:
+for (cell_type in cell_types){
+  cluster_cell <- subset(experiment.aggregate, idents = cell_type)
+  expr_cell <- as.matrix(GetAssayData(cluster_cell))
+  # Filter out genes that are 0 for every cell in this cluster
+  bad_cell <- which(rowSums(expr_cell) == 0)
+  expr_cell <- expr_cell[-bad_cell,]
+  mm_cell <- model.matrix(~0 + orig.ident, data = cluster_cell@meta.data)
+  # Fit the model
+  fit_cell <- lmFit(expr_cell, mm_cell)
+  # Means in each sample for each gene
+  head(coef(fit_cell)) 
+  # Contrast WT-MUT accounting for repliicates
+  contr_cell<- makeContrasts(c(orig.identWT_F_E18_WB1+orig.identWT_F_E18_WB2) - c(orig.identMUT_F_E18_WB1+orig.identMUT_F_E18_WB2), levels = colnames(coef(fit_cell)))
+  tmp_cell <- contrasts.fit(fit_cell, contrasts = contr_cell)
+  # Use empirical Bayes to calculate the t-statistics
+  tmp_cell <- eBayes(tmp_cell)
+  # Find top 1000000 DE genes (should cover all genes)
+  cell_toptable <- topTable(tmp_cell, sort.by = "P", n = 1000000) 
+  # Subset data to remove all non-significant genes
+  cell_Limma_DEG <- subset(x = cell_toptable, subset = adj.P.Val < 0.05)
+  # Write data to CSV so analysis does not need to be rerun when working with data
+  write.csv(cell_Limma_DEG, file = glue(DEG_data_dir, cell_type, "_", metadata_info, "_Limma_DEG.csv"))
+}
 
 # For MALES
 # To get all genes:
@@ -88,24 +116,24 @@ experiment.aggregate <- RenameIdents(object = experiment.aggregate, 'Non-neurona
 
 # For FEMALES
 # To get all genes:
-for (cell_type in cell_types){
-  cluster_cell <- subset(experiment.aggregate, idents = cell_type)
-  expr_cell <- as.matrix(GetAssayData(cluster_cell))
-  # Filter out genes that are 0 for every cell in this cluster
-  bad_cell <- which(rowSums(expr_cell) == 0)
-  expr_cell <- expr_cell[-bad_cell,]
-  mm_cell <- model.matrix(~0 + orig.ident, data = cluster_cell@meta.data)
-  # Fit the model
-  fit_cell <- lmFit(expr_cell, mm_cell)
-  # Means in each sample for each gene
-  head(coef(fit_cell)) 
-  # Contrast WT-MUT accounting for repliicates
-  contr_cell<- makeContrasts(c(orig.identWT_F_E18_WB1+orig.identWT_F_E18_WB2) - c(orig.identMUT_F_E18_WB1+orig.identMUT_F_E18_WB2), levels = colnames(coef(fit_cell)))
-  tmp_cell <- contrasts.fit(fit_cell, contrasts = contr_cell)
-  # Use empirical Bayes to calculate the t-statistics
-  tmp_cell <- eBayes(tmp_cell)
-  # Find top 1000000 DE genes (should cover all genes)
-  cell_toptable <- topTable(tmp_cell, sort.by = "P", n = 1000000) 
-  # Write data to CSV so analysis does not need to be rerun when working with data
-  write.csv(cell_toptable, file = glue(DEG_data_dir_total_genes, metadata_info, "/", cell_type, "_", metadata_info, "_Limma_DEG.csv"))
-}
+#for (cell_type in cell_types){
+#  cluster_cell <- subset(experiment.aggregate, idents = cell_type)
+#  expr_cell <- as.matrix(GetAssayData(cluster_cell))
+#  # Filter out genes that are 0 for every cell in this cluster
+#  bad_cell <- which(rowSums(expr_cell) == 0)
+#  expr_cell <- expr_cell[-bad_cell,]
+#  mm_cell <- model.matrix(~0 + orig.ident, data = cluster_cell@meta.data)
+#  # Fit the model
+#  fit_cell <- lmFit(expr_cell, mm_cell)
+#  # Means in each sample for each gene
+#  head(coef(fit_cell)) 
+#  # Contrast WT-MUT accounting for repliicates
+#  contr_cell<- makeContrasts(c(orig.identWT_F_E18_WB1+orig.identWT_F_E18_WB2) - c(orig.identMUT_F_E18_WB1+orig.identMUT_F_E18_WB2), levels = colnames(coef(fit_cell)))
+#  tmp_cell <- contrasts.fit(fit_cell, contrasts = contr_cell)
+#  # Use empirical Bayes to calculate the t-statistics
+#  tmp_cell <- eBayes(tmp_cell)
+#  # Find top 1000000 DE genes (should cover all genes)
+#  cell_toptable <- topTable(tmp_cell, sort.by = "P", n = 1000000) 
+#  # Write data to CSV so analysis does not need to be rerun when working with data
+#  write.csv(cell_toptable, file = glue(DEG_data_dir_total_genes, metadata_info, "/", cell_type, "_", metadata_info, "_Limma_DEG.csv"))
+#}
