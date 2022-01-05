58f6106fcd18eee29ed80a51ccae1c1c581d96c3
Viktoria Haghani <vhaghani@ucdavis.edu>
2022-01-05 10:16:03 -0800

    change to nested for loop

--- scripts/04_GO_analysis/GO_analysis_all_cell_types.R
+++ scripts/04_GO_analysis/GO_analysis_all_cell_types.R
@@ -15,15 +15,18 @@ go_data_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/"
 figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/"
 
 # Lists
-concise_metadatas <- list("M_MUT_and_WT_M_E18_WB", "M_MUT_and_WT_M_P30_CORT", "M_MUT_and_WT_M_P60_CORT", "M_MUT_and_WT_M_P120_CORT")
+time_points <- list("E18_WB", "P30_CORT", "P60_CORT", "P120_CORT")
 cell_types <- list("L2_3_IT", "L6", "Sst", "L5", "L4", "Pvalb", "Sncg", "Non_neuronal", "Oligo", "Vip", "Lamp5", "Astro", "Peri", "Endo")
-ont_types <- list("BP", "CC", "MF")
+go_ontologies <- list("BP", "CC", "MF")
 
 ################################################################################
 
 # Read in GO Data
-foreach(met = concise_metadatas, cell_type = cell_types, ont = ont_types) %do% {
-  print("placeholder")
-  # I want it to look like: L6_P30_CORT
-  # Can use assign(glue(variable_name), function)
+
+for (time_point in time_points){
+  for (Cell_type in cell_types){
+    for (ont in go_ontologies){
+      assign(glue(cell_type, "_", time_point, "_", ont), read.csv())
+    }
+  }
 }
