c0cdf96838eed31905d7d94251bbeba66c6bb01d
Viktoria Haghani <vhaghani@ucdavis.edu>
2022-01-05 10:24:19 -0800

    read in GO data

--- scripts/04_GO_analysis/GO_analysis_all_cell_types.R
+++ scripts/04_GO_analysis/GO_analysis_all_cell_types.R
@@ -19,14 +19,16 @@ time_points <- list("E18_WB", "P30_CORT", "P60_CORT", "P120_CORT")
 cell_types <- list("L2_3_IT", "L6", "Sst", "L5", "L4", "Pvalb", "Sncg", "Non_neuronal", "Oligo", "Vip", "Lamp5", "Astro", "Peri", "Endo")
 go_ontologies <- list("BP", "CC", "MF")
 
+# Variable
+metadata <- "M_MUT_and_WT_M"
+
 ################################################################################
 
 # Read in GO Data
-
 for (time_point in time_points){
-  for (Cell_type in cell_types){
+  for (cell_type in cell_types){
     for (ont in go_ontologies){
-      assign(glue(cell_type, "_", time_point, "_", ont), read.csv())
+      assign(glue(cell_type, "_", time_point, "_", ont), read.csv(file = glue(go_data_path, metadata, "_", time_point, "/", cell_type, "_", metadata, "_", time_point, "_", ont, "_gentable.csv")))
     }
   }
 }
