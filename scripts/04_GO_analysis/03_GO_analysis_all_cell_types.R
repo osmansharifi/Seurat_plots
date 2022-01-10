3f0543d0ecc824ee168913b0512a8856d5de2539
Osman <osharifi@ucdavis.edu>
2022-01-10 14:34:37 -0800

    update

--- scripts/04_GO_analysis/03_GO_analysis_all_cell_types.R
+++ scripts/04_GO_analysis/03_GO_analysis_all_cell_types.R
@@ -19,51 +19,21 @@ plot_subtitle <- "All Cell Types, Time Points, and Ontologies for Male Mice"
 # Read in GO Data from master CSV file
 go_data <- read.csv(file = csv_path)
 
-# Visualize GO Data
-
-ggplot(go_data, aes(x = Metadata, y = Term, size = -log10(go_data[9]))) +
-  expand_limits(y = 1) +
-  geom_point(shape = 21) +
-  scale_size(range = c(2.5,12.5)) +
-  scale_fill_continuous(low = 'royalblue', high = 'red4') +
-  xlab('') +
-  ylab('Enrichment score') +
-  labs(title = plot_title, subtitle = plot_subtitle) +
-  geom_hline(yintercept = c(-log10(0.05), -log10(0.01), -log10(0.001)),
-             linetype = c("dotted", "longdash", "solid"),
-             colour = c("black", "black", "black"),
-             size = c(0.5, 1.5, 3)) +
-  theme_bw(base_size = 24) +
-  theme(
-    legend.position = 'right',
-    legend.background = element_rect(),
-    plot.title = element_text(angle = 0, size = 16, face = 'bold', vjust = 1),
-    plot.subtitle = element_text(angle = 0, size = 14, face = 'bold', vjust = 1),
-    plot.caption = element_text(angle = 0, size = 12, face = 'bold', vjust = 1),
-    axis.text.x = element_text(angle = 0, size = 12, face = 'bold', hjust = 1.10),
-    axis.text.y = element_text(angle = 0, size = 12, face = 'bold', vjust = 0.5),
-    axis.title = element_text(size = 12, face = 'bold'),
-    axis.title.x = element_text(size = 12, face = 'bold'),
-    axis.title.y = element_text(size = 12, face = 'bold'),
-    axis.line = element_line(colour = 'black'),
-    # Legend
-    legend.key = element_blank(), # removes the border
-    legend.key.size = unit(1, "cm"), # Sets overall area/size of the legend
-    legend.text = element_text(size = 14, face = "bold"), # Text size
-    title = element_text(size = 14, face = "bold")) +
-  coord_flip()
-
-#ggplot2::ggsave(glue('{figure_path}master_go_data_Fisher.pdf'),
-#                device = NULL,
-#                height = 8.5,
-#                width = 12)
-
-# plot: dot plot
+#subset and sort GO terms
 E18_GO <- go_data[which(go_data$Time.Point=='E18'),]
+E18_GO <- E18_GO[order(E18_GO$Fisher),]
+for (celltype in E18_GO$Cell.Type){
+  order(E18_GO$Fisher)
+  return(E18_GO)
+}
 P30_GO <- go_data[which(go_data$Time.Point=='P30'),]
+P30_GO <- P30_GO[order(P30_GO$Fisher),]
 P60_GO <- go_data[which(go_data$Time.Point=='P60'),]
+P60_GO <- P60_GO[order(P60_GO$Fisher),]
 P120_GO <- go_data[which(go_data$Time.Point=='P120'),]
+P120_GO <- P120_GO[order(P120_GO$Fisher),]
 
+# Visualize GO Data
 p1<-ggplot(data = E18_GO, aes(x = Cell.Type, y = Term, 
                         color = -log10(`Fisher`), size = Significant)) + 
   geom_point() +
@@ -119,3 +89,42 @@ ggsave(p4,
        filename = "/Users/osman/Documents/GitHub/snRNA-seq-pipeline/figures/go_analysis/P120_male_GO.pdf",
        height = 20, 
        width = 10)
+
+
+#########################################################################################################
+ggplot(go_data, aes(x = Metadata, y = Term, size = -log10(go_data[9]))) +
+  expand_limits(y = 1) +
+  geom_point(shape = 21) +
+  scale_size(range = c(2.5,12.5)) +
+  scale_fill_continuous(low = 'royalblue', high = 'red4') +
+  xlab('') +
+  ylab('Enrichment score') +
+  labs(title = plot_title, subtitle = plot_subtitle) +
+  geom_hline(yintercept = c(-log10(0.05), -log10(0.01), -log10(0.001)),
+             linetype = c("dotted", "longdash", "solid"),
+             colour = c("black", "black", "black"),
+             size = c(0.5, 1.5, 3)) +
+  theme_bw(base_size = 24) +
+  theme(
+    legend.position = 'right',
+    legend.background = element_rect(),
+    plot.title = element_text(angle = 0, size = 16, face = 'bold', vjust = 1),
+    plot.subtitle = element_text(angle = 0, size = 14, face = 'bold', vjust = 1),
+    plot.caption = element_text(angle = 0, size = 12, face = 'bold', vjust = 1),
+    axis.text.x = element_text(angle = 0, size = 12, face = 'bold', hjust = 1.10),
+    axis.text.y = element_text(angle = 0, size = 12, face = 'bold', vjust = 0.5),
+    axis.title = element_text(size = 12, face = 'bold'),
+    axis.title.x = element_text(size = 12, face = 'bold'),
+    axis.title.y = element_text(size = 12, face = 'bold'),
+    axis.line = element_line(colour = 'black'),
+    # Legend
+    legend.key = element_blank(), # removes the border
+    legend.key.size = unit(1, "cm"), # Sets overall area/size of the legend
+    legend.text = element_text(size = 14, face = "bold"), # Text size
+    title = element_text(size = 14, face = "bold")) +
+  coord_flip()
+
+#ggplot2::ggsave(glue('{figure_path}master_go_data_Fisher.pdf'),
+#                device = NULL,
+#                height = 8.5,
+#                width = 12)
