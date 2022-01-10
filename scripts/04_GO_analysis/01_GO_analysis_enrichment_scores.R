996b4dc2164fc24fa864cb53f7730a93c6e37b3f
Viktoria Haghani <vhaghani@ucdavis.edu>
2022-01-10 15:44:37 -0800

    run e18 top3

--- scripts/04_GO_analysis/01_GO_analysis_enrichment_scores.R
+++ scripts/04_GO_analysis/01_GO_analysis_enrichment_scores.R
@@ -12,18 +12,18 @@ library(tidyverse)
 ## Variables
 
 ## Paths
-#Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_M_E18_WB/"
-Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_M_P30_CORT/"
+Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_M_E18_WB/"
+#Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_M_P30_CORT/"
 #Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_M_P60_CORT/"
 #Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_M_P120_CORT/"
 
-#figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_M_E18_WB/"
-figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_M_P30_CORT/"
+figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_M_E18_WB/"
+#figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_M_P30_CORT/"
 #figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_M_P60_CORT/"
 #figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_M_P120_CORT/"
 
-#gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_M_E18_WB/"
-gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_M_P30_CORT/"
+gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_M_E18_WB/"
+#gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_M_P30_CORT/"
 #gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_M_P60_CORT/"
 #gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_M_P120_CORT/"
 
@@ -32,13 +32,13 @@ cell_types <- list("L2_3_IT", "L6", "Sst", "L5", "L4", "Pvalb", "Sncg", "Non_neu
 topgo_ontologies <- list("BP", "CC", "MF")
 
 ## Other variables
-#metadata_info_concise <- "M_MUT_and_WT_M_E18_WB"
-metadata_info_concise <- "M_MUT_and_WT_M_P30_CORT"
+metadata_info_concise <- "M_MUT_and_WT_M_E18_WB"
+#metadata_info_concise <- "M_MUT_and_WT_M_P30_CORT"
 #metadata_info_concise <- "M_MUT_and_WT_M_P60_CORT"
 #metadata_info_concise <- "M_MUT_and_WT_M_P120_CORT"
 
-#metadata_info_expanded <- "Male, E18, Whole Brain"
-metadata_info_expanded <- "Male, P30, Cortex"
+metadata_info_expanded <- "Male, E18, Whole Brain"
+#metadata_info_expanded <- "Male, P30, Cortex"
 #metadata_info_expanded <- "Male, P60, Cortex"
 #metadata_info_expanded <- "Male, P120, Cortex"
 ################################################################################
@@ -195,3 +195,4 @@ for (cell_type in cell_types){
     write.csv(GenTable, file = glue(gentable_path, cell_type, "_", metadata_info_concise, "_", ont, "_top3_gentable.csv"))
   }
 }
+
