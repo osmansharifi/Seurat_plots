32cf1c5b7e8fedbb4d130f9ad363fe12d58cc4bc
Viktoria Haghani <vhaghani@ucdavis.edu>
2022-01-12 14:43:23 -0800

    run female P150 topGO

--- scripts/04_GO_analysis/01_GO_analysis_enrichment_scores.R
+++ scripts/04_GO_analysis/01_GO_analysis_enrichment_scores.R
@@ -18,8 +18,8 @@ library(tidyverse)
 #Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_M_P120_CORT/"
 #Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_F_E18_WB/"
 #Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_F_P30_CORT/"
-Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_F_P60_CORT/"
-#Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_F_P150_CORT/"
+#Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_F_P60_CORT/"
+Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_and_WT_F_P150_CORT/"
 
 #figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_M_E18_WB/"
 #figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_M_P30_CORT/"
@@ -27,8 +27,8 @@ Limma_DEG_dir <- "~/GitHub/snRNA-seq-pipeline/DEG_data/total_genes/Limma/M_MUT_a
 #figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_M_P120_CORT/"
 #figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_F_E18_WB/"
 #figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_F_P30_CORT/"
-figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_F_P60_CORT/"
-#figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_F_P150_CORT/"
+#figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_F_P60_CORT/"
+figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_scores/M_MUT_and_WT_F_P150_CORT/"
 
 #gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_M_E18_WB/"
 #gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_M_P30_CORT/"
@@ -36,11 +36,12 @@ figure_path <- "~/GitHub/snRNA-seq-pipeline/figures/go_analysis/enrichment_score
 #gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_M_P120_CORT/"
 #gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_F_E18_WB/"
 #gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_F_P30_CORT/"
-gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_F_P60_CORT/"
-#gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_F_P150_CORT/"
+#gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_F_P60_CORT/"
+gentable_path <- "~/GitHub/snRNA-seq-pipeline/GO_data/GO_term_tables/M_MUT_and_WT_F_P150_CORT/"
 
 ## Lists
-cell_types <- list("Vip") #"L2_3_IT", "L6", "Sst", "L5", "L4", "Pvalb", "Sncg", "Non_neuronal", "Oligo", "Vip", "Lamp5", "Astro") #, "Peri", "Endo")
+#cell_types <- list("L2_3_IT", "L6", "Sst", "L5", "L4", "Pvalb", "Sncg", "Non_neuronal", "Oligo", "Vip", "Lamp5", "Astro", "Peri", "Endo")
+cell_types <- list("L2_3_IT", "Sst", "L5", "L4", "Pvalb", "Sncg", "Non_neuronal", "Oligo", "Vip", "Astro")
 topgo_ontologies <- list("BP", "CC", "MF")
 
 ## Other variables
@@ -50,8 +51,8 @@ topgo_ontologies <- list("BP", "CC", "MF")
 #metadata_info_concise <- "M_MUT_and_WT_M_P120_CORT"
 #metadata_info_concise <- "M_MUT_and_WT_F_E18_WB"
 #metadata_info_concise <- "M_MUT_and_WT_F_P30_CORT"
-metadata_info_concise <- "M_MUT_and_WT_F_P60_CORT"
-#metadata_info_concise <- "M_MUT_and_WT_F_P150_CORT"
+#metadata_info_concise <- "M_MUT_and_WT_F_P60_CORT"
+metadata_info_concise <- "M_MUT_and_WT_F_P150_CORT"
 
 #metadata_info_expanded <- "Male, E18, Whole Brain"
 #metadata_info_expanded <- "Male, P30, Cortex"
@@ -59,8 +60,9 @@ metadata_info_concise <- "M_MUT_and_WT_F_P60_CORT"
 #metadata_info_expanded <- "Male, P120, Cortex"
 #metadata_info_expanded <- "Female, E18, Whole Brain"
 #metadata_info_expanded <- "Female, P30, Cortex"
-metadata_info_expanded <- "Female, P60, Cortex"
-#metadata_info_expanded <- "Female, P150, Cortex"
+#metadata_info_expanded <- "Female, P60, Cortex"
+metadata_info_expanded <- "Female, P150, Cortex"
+
 ################################################################################
 
 
