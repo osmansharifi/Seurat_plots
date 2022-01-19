eabf2a22d717c9e1aa407ac91da75ba6a6ee9e01
Osman <osharifi@ucdavis.edu>
2022-01-19 00:22:02 -0800

    Update modified_fastq.py

--- data_preparation/modified_fastq.py
+++ data_preparation/modified_fastq.py
@@ -2,7 +2,7 @@
 import csv
 import sys
 import gzip
-
+'''
 mecp2_WT = "TCACCTAAGGGCCCCGGGCTTTATTACTCTCCCCTACGCTAATCGGGTCCTAGTGGGCAATGTCTGCGGCGTACTGA\
 TAGAAGTTACAGGTGCGTGGTCCCGAGAGCAATCGCAAAACACGCAGCATTGTAAGGAAGGACAATCGAACAAGCGCGTAGATATAATA\
 ACCACGGGCTTGGGCTGCTGGCGTTCGCTACGTTGCTAGGGCCGTTGTGCCACTAGTACTATGACCTTCCCTCGGGTCTGTAGTGTGGA\
@@ -24,7 +24,7 @@ data_preparation/fake_barcode_counts.csv','r') as csvfile:
 	readit = csv.reader(csvfile)
 	for line in readit:
 		print(line) # shows barcode file contents
-
+'''
 
 #read fastq files
 input_file = ('/Users/osman/Documents/GitHub/snRNA-seq-pipeline/data_preparation/fastq_subset/190106Csub_S9_L001_R1_001.fastq.gz')
@@ -33,32 +33,9 @@ if input_file.endswith('.gz'):
 	fp = gzip.open(input_file, 'rt')
 else:
 	fp = open(input_file, 'r')
+	print(fp)
 
 while True: 
 	description = fp.readline()
 	sequence = fp.readline() 
-	extra1 = fp.readline()
-	extra2 = fp.readline()
-	#if description == '':
-		#break
-	#description = description.replace('@', '>', 1)
 	print(description + sequence)
-	
-#fastq_file = open(('/Users/osman/Documents/GitHub/snRNA-seq-pipeline/data_preparation/fastq_subset/190106Csub_S9_L001_R1_001.fastq.gz'), 'r')
-#print(fastq_file)
-'''
-def readFastq(filename):
-	fastq_open = open(filename, "rU")     
-	for line in fastq_open:
-    	header1 = line.rstrip()
-        sequences = next(fastq_open).rstrip()
-        header2 = next(fastq_open).rstrip()
-        qualities = next(fastq_open).rstrip()
-        if len(seq) == 0:
-        	break
-        return sequences, qualities
-    fastq_open.close()
-
-seqs, quals = readFastq('/Users/osman/Documents/GitHub/snRNA-seq-pipeline/data_preparation/fastq_subset/190106Csub_S9_L001_R1_001.fastq.gz')
-print (seqs)
-'''
