d7d4c0a4dc78b1ce5be4546f95f3d34d5f1a9f59
Osman <osharifi@ucdavis.edu>
2022-01-24 14:45:40 -0800

    dotplots

--- scripts/04_GO_analysis/03_GO_analysis_all_cell_types.R
+++ scripts/04_GO_analysis/03_GO_analysis_all_cell_types.R
@@ -27,6 +27,7 @@ p.adjust <- round(p.adjust(E18_GO$Fisher,method="BH"),digits = 4)
 E18_GO=cbind(E18_GO,p.adjust)
 E18_GO <- E18_GO[order(E18_GO$p.adjust),]
 E18_GO <- filter(E18_GO, p.adjust < 0.05)
+E18_GO$GeneRatio <- E18_GO$Significant/E18_GO$Annotated
 
 P30_GO <- go_data[which(go_data$Time.Point=='P30'),]
 P30_GO <- P30_GO[order(P30_GO$Fisher),]
@@ -34,87 +35,37 @@ p.adjust <- round(p.adjust(P30_GO$Fisher,method="BH"),digits = 4)
 P30_GO=cbind(P30_GO,p.adjust)
 P30_GO <- P30_GO[order(P30_GO$p.adjust),]
 P30_GO <- filter(P30_GO, p.adjust < 0.05)
-
+P30_GO$GeneRatio <- P30_GO$Significant/P30_GO$Annotated
 
 P60_GO <- go_data[which(go_data$Time.Point=='P60'),]
 P60_GO <- P60_GO[order(P60_GO$Fisher),]
+p.adjust <- round(p.adjust(P60_GO$Fisher,method="BH"),digits = 4)
+P60_GO=cbind(P60_GO,p.adjust)
+P60_GO <- P60_GO[order(P60_GO$p.adjust),]
+P60_GO <- filter(P60_GO, p.adjust < 0.05)
+P60_GO$GeneRatio <- P60_GO$Significant/P60_GO$Annotated
+P60_GO <- P60_GO[order(P60_GO$GeneRatio),]
+
 P120_GO <- go_data[which(go_data$Time.Point=='P120'),]
 P120_GO <- P120_GO[order(P120_GO$Fisher),]
+p.adjust <- round(p.adjust(P120_GO$Fisher,method="BH"),digits = 4)
+P120_GO=cbind(P120_GO,p.adjust)
+P120_GO <- P120_GO[order(P120_GO$p.adjust),]
+P120_GO <- filter(P120_GO, p.adjust < 0.05)
+P120_GO$GeneRatio <- P120_GO$Significant/P120_GO$Annotated
+
 
 # Visualize GO Data
-p1<-ggplot(data = E18_GO, aes(x = Cell.Type, y = Term, 
-                        color = p.adjust, size = Significant)) + 
-  geom_point() +
-  scale_color_gradient(low = "red", high = "blue") +
-  theme_bw() + 
-  ylab("") + 
-  xlab("") + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
-  theme(axis.text.y = element_text(angle = 0, vjust = 0.5, hjust=1))+
-  ggtitle("TopGO E18 male GO enrichment analysis")
-ggsave(p1,
-       filename = "/Users/osman/Documents/GitHub/snRNA-seq-pipeline/figures/go_analysis/E18_male_GO.pdf",
-       height = 15, 
-       width = 10)
-
-p2<-ggplot(data = P30_GO, aes(x = Cell.Type, y = Term, 
-                              color = -log10(`Fisher`), size = Significant)) + 
-  geom_point() +
-  scale_color_gradient(low = "blue", high = "red") +
-  theme_bw() + 
-  ylab("") + 
-  xlab("") + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
-  ggtitle("TopGO P30 male GO enrichment analysis")
-ggsave(p2,
-       filename = "/Users/osman/Documents/GitHub/snRNA-seq-pipeline/figures/go_analysis/P30_male_GO.pdf",
-       height = 20, 
-       width = 10)
-
-p3<-ggplot(data = P60_GO, aes(x = Cell.Type, y = Term, 
-                              color = -log10(`Fisher`), size = Significant)) + 
-  geom_point() +
-  scale_color_gradient(low = "blue", high = "red") +
-  theme_bw() + 
-  ylab("") + 
-  xlab("") + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
-  ggtitle("TopGO P60 male GO enrichment analysis")
-ggsave(p3,
-       filename = "/Users/osman/Documents/GitHub/snRNA-seq-pipeline/figures/go_analysis/P60_male_GO.pdf",
-       height = 20, 
-       width = 10)
-
-p4<-ggplot(data = P120_GO, aes(x = Cell.Type, y = Term, 
-                              color = -log10(`Fisher`), size = Significant)) + 
-  geom_point() +
-  scale_color_gradient(low = "blue", high = "red") +
-  theme_bw() + 
-  ylab("") + 
-  xlab("") + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
-  ggtitle("TopGO P120 male GO enrichment analysis")
-ggsave(p4,
-       filename = "/Users/osman/Documents/GitHub/snRNA-seq-pipeline/figures/go_analysis/P120_male_GO.pdf",
-       height = 20, 
-       width = 10)
-
-
 #########################################################################################################
-E18_GO$GeneRatio <- E18_GO$Significant/E18_GO$Annotated # fixes order
 gg1 <- ggplot(E18_GO,
               aes(x = Term, y = Cell.Type, size = GeneRatio, fill = p.adjust))  +
-  
-  # expand_limits(y = 1) +
   geom_point(shape = 21) +
   scale_size(range = c(2.5,12.5)) +
   scale_fill_viridis() + 
-  # scale_fill_continuous(low = 'royalblue', high = 'red4') +
-  
   xlab('') + ylab('Cell Type') +
   labs(
     title = 'FDR corrected TopGO Terms',
-    subtitle = 'Significant E18 Male terms'
+    subtitle = 'Significant E18 Male '
   )  +   
   
   theme_bw(base_size = 24) +
@@ -140,4 +91,110 @@ gg1 <- ggplot(E18_GO,
   coord_flip()
 ggsave("E18_Male_GOTerms_dotplot.pdf",gg1, width = 15,
        height = 10)
+#P30
+gg2 <- ggplot(P30_GO,
+              aes(x = Term, y = Cell.Type, size = GeneRatio, fill = p.adjust))  +
+  geom_point(shape = 21) +
+  scale_size(range = c(2.5,12.5)) +
+  scale_fill_viridis() + 
+  xlab('') + ylab('Cell Type') +
+  labs(
+    title = 'FDR corrected TopGO Terms',
+    subtitle = 'Significant P30 Male'
+  )  +   
+  
+  theme_bw(base_size = 24) +
+  theme(
+    legend.position = 'right',
+    legend.background = element_rect(),
+    plot.title = element_text(angle = 0, size = 16, face = 'bold', vjust = 1),
+    plot.subtitle = element_text(angle = 0, size = 14, face = 'bold', vjust = 1),
+    plot.caption = element_text(angle = 0, size = 12, face = 'bold', vjust = 1),
+    
+    axis.text.x = element_text(angle = 90, size = 12, face = 'bold', hjust = 1.0, vjust = 0.5),
+    axis.text.y = element_text(angle = 0, size = 12, face = 'bold', vjust = 0.5),
+    axis.title = element_text(size = 12, face = 'bold'),
+    axis.title.x = element_text(size = 12, face = 'bold'),
+    axis.title.y = element_text(size = 12, face = 'bold'),
+    axis.line = element_line(colour = 'black'),
+    
+    #Legend
+    legend.key = element_blank(), # removes the border
+    legend.key.size = unit(1, "cm"), # Sets overall area/size of the legend
+    legend.text = element_text(size = 14, face = "bold"), # Text size
+    title = element_text(size = 14, face = "bold")) +
+  coord_flip()
+ggsave("P30_Male_GOTerms_dotplot.pdf",gg2, width = 15,
+       height = 10)
 
+#P60
+gg3 <- ggplot(P60_GO,
+              aes(x = Term, y = Cell.Type, size = GeneRatio, fill = p.adjust))  +
+  geom_point(shape = 21) +
+  scale_size(range = c(2.5,12.5)) +
+  scale_fill_viridis() + 
+  xlab('') + ylab('Cell Type') +
+  labs(
+    title = 'FDR corrected TopGO Terms',
+    subtitle = 'Significant P60 Male'
+  )  +   
+  
+  theme_bw(base_size = 24) +
+  theme(
+    legend.position = 'right',
+    legend.background = element_rect(),
+    plot.title = element_text(angle = 0, size = 16, face = 'bold', vjust = 1),
+    plot.subtitle = element_text(angle = 0, size = 14, face = 'bold', vjust = 1),
+    plot.caption = element_text(angle = 0, size = 12, face = 'bold', vjust = 1),
+    
+    axis.text.x = element_text(angle = 90, size = 12, face = 'bold', hjust = 1.0, vjust = 0.5),
+    axis.text.y = element_text(angle = 0, size = 12, face = 'bold', vjust = 0.5),
+    axis.title = element_text(size = 12, face = 'bold'),
+    axis.title.x = element_text(size = 12, face = 'bold'),
+    axis.title.y = element_text(size = 12, face = 'bold'),
+    axis.line = element_line(colour = 'black'),
+    
+    #Legend
+    legend.key = element_blank(), # removes the border
+    legend.key.size = unit(1, "cm"), # Sets overall area/size of the legend
+    legend.text = element_text(size = 14, face = "bold"), # Text size
+    title = element_text(size = 14, face = "bold")) +
+  coord_flip()
+ggsave("P60_Male_GOTerms_dotplot.pdf",gg3, width = 15,
+       height = 10)
+
+#P120
+gg4 <- ggplot(P120_GO,
+              aes(x = Term, y = Cell.Type, size = GeneRatio, fill = p.adjust))  +
+  geom_point(shape = 21) +
+  scale_size(range = c(2.5,12.5)) +
+  scale_fill_viridis() + 
+  xlab('') + ylab('Cell Type') +
+  labs(
+    title = 'FDR corrected TopGO Terms',
+    subtitle = 'Significant P120 Male'
+  )  +   
+  
+  theme_bw(base_size = 24) +
+  theme(
+    legend.position = 'right',
+    legend.background = element_rect(),
+    plot.title = element_text(angle = 0, size = 16, face = 'bold', vjust = 1),
+    plot.subtitle = element_text(angle = 0, size = 14, face = 'bold', vjust = 1),
+    plot.caption = element_text(angle = 0, size = 12, face = 'bold', vjust = 1),
+    
+    axis.text.x = element_text(angle = 90, size = 12, face = 'bold', hjust = 1.0, vjust = 0.5),
+    axis.text.y = element_text(angle = 0, size = 12, face = 'bold', vjust = 0.5),
+    axis.title = element_text(size = 12, face = 'bold'),
+    axis.title.x = element_text(size = 12, face = 'bold'),
+    axis.title.y = element_text(size = 12, face = 'bold'),
+    axis.line = element_line(colour = 'black'),
+    
+    #Legend
+    legend.key = element_blank(), # removes the border
+    legend.key.size = unit(1, "cm"), # Sets overall area/size of the legend
+    legend.text = element_text(size = 14, face = "bold"), # Text size
+    title = element_text(size = 14, face = "bold")) +
+  coord_flip()
+ggsave("P120_Male_GOTerms_dotplot.pdf",gg4, width = 15,
+       height = 10)
