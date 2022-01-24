621a6ee017ba67e1f46ce4d1c90c50bbe5e766fc
Osman <osharifi@ucdavis.edu>
2022-01-24 13:39:13 -0800

    dotplot update

--- scripts/04_GO_analysis/03_GO_analysis_all_cell_types.R
+++ scripts/04_GO_analysis/03_GO_analysis_all_cell_types.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+library(viridis)
 # By Osman Sharifi & Viktoria Haghani
 
 ################################################################################
@@ -100,18 +101,23 @@ ggsave(p4,
 
 
 #########################################################################################################
-ggplot(go_data, aes(x = Metadata, y = Term, size = -log10(go_data[9]))) +
-  expand_limits(y = 1) +
+E18_GO = E18_GO[order(-E18_GO$p.adjust),] 
+E18_GO$GeneRatio <- E18_GO$Significant/E18_GO$Annotated # fixes order
+gg1 <- ggplot(E18_GO,
+              aes(x = Term, y = Cell.Type, size = (GeneRatio), fill = p.adjust))  +
+  
+  # expand_limits(y = 1) +
   geom_point(shape = 21) +
   scale_size(range = c(2.5,12.5)) +
-  scale_fill_continuous(low = 'royalblue', high = 'red4') +
-  xlab('') +
-  ylab('Enrichment score') +
-  labs(title = plot_title, subtitle = plot_subtitle) +
-  geom_hline(yintercept = c(-log10(0.05), -log10(0.01), -log10(0.001)),
-             linetype = c("dotted", "longdash", "solid"),
-             colour = c("black", "black", "black"),
-             size = c(0.5, 1.5, 3)) +
+  scale_fill_viridis() + 
+  # scale_fill_continuous(low = 'royalblue', high = 'red4') +
+  
+  xlab('') + ylab('Cell Type') +
+  labs(
+    title = 'TopGO Terms',
+    subtitle = 'Curated 20 terms ordered by FDR corrected p-value'
+  )  +   
+  
   theme_bw(base_size = 24) +
   theme(
     legend.position = 'right',
@@ -119,20 +125,22 @@ ggplot(go_data, aes(x = Metadata, y = Term, size = -log10(go_data[9]))) +
     plot.title = element_text(angle = 0, size = 16, face = 'bold', vjust = 1),
     plot.subtitle = element_text(angle = 0, size = 14, face = 'bold', vjust = 1),
     plot.caption = element_text(angle = 0, size = 12, face = 'bold', vjust = 1),
-    axis.text.x = element_text(angle = 0, size = 12, face = 'bold', hjust = 1.10),
+    
+    axis.text.x = element_text(angle = 90, size = 12, face = 'bold', hjust = 1.10),
     axis.text.y = element_text(angle = 0, size = 12, face = 'bold', vjust = 0.5),
     axis.title = element_text(size = 12, face = 'bold'),
     axis.title.x = element_text(size = 12, face = 'bold'),
     axis.title.y = element_text(size = 12, face = 'bold'),
     axis.line = element_line(colour = 'black'),
-    # Legend
+    
+    #Legend
     legend.key = element_blank(), # removes the border
     legend.key.size = unit(1, "cm"), # Sets overall area/size of the legend
     legend.text = element_text(size = 14, face = "bold"), # Text size
     title = element_text(size = 14, face = "bold")) +
+  
+  
   coord_flip()
+ggsave("E18_Male_GOTerms_dotplot.pdf",gg1, width = 15,
+       height = 10)
 
-#ggplot2::ggsave(glue('{figure_path}master_go_data_Fisher.pdf'),
-#                device = NULL,
-#                height = 8.5,
-#                width = 12)
