e73aa2fac48181ea9ce1e64b08f7e20cc20b1938
Viktoria Haghani <vhaghani@ucdavis.edu>
2022-02-17 15:39:45 -0800

    read in sequence.alleler and combine info to one output tsv

--- data_preparation/append_alleler.py
+++ data_preparation/append_alleler.py
@@ -1,25 +1,53 @@
 import os
+import pandas
+import csv
 
-##################
-## Append Files ##
-##################
+# Path to file output
+output_file_path = "."
 
-# Creating a list of filenames
-#filenames = ['file_1.txt', 'file_2.txt'] 
-filenames = os.listdir('/Users/osman/Desktop/plot_test/alleler_files') 
-# Open file3 in write mode
-with open('file_3.txt', 'w') as outfile:
-  
-    # Iterate through list
-    for names in filenames:
-  
-        # Open each file in read mode
-        with open(names) as infile:
-  
-            # read the data from file1 and
-            # file2 and write it in file3
-            outfile.write(infile.read())
-  
-        # Add '\n' to enter data of file2
-        # from next line
-        outfile.write("")
+# Mutation status
+mut_stat = ["MUT", "WT"]
+
+# Sex
+sexes = ["M", "F"]
+
+# Time Points
+time_points = ["E18", "P30", "P60", "P120", "P150"]
+
+# Tissues
+tissue_types = ["WB", "CORT", "CORT", "CORT", "CORT"]
+
+# Replicates
+replicates = ["1", "2", "3", "4"]
+
+# Make a master data frame to combine all the data from each individual sequence.alleler file
+master_sequence_alleler = pd.DataFrame()
+
+for mut in mut_stat:
+    for sex in sexes:
+        for time_point, tissue in zip(time_points, tissue_types):
+            for rep in replicates:
+                # Check if file exists (since males and females have different time points and time points may have different replicates)
+                my_file = f'/share/lasallelab/Osman/cell_parsing_test/{mut}_{sex}_{time_point}_{tissue}{rep}/sequence.alleler'
+                # Store True if file exists
+                isExist = os.path.isfile(my_file)
+                if isExist = 
+                    with open(f'{my_file}') as fp:
+                        # Read in the file contents
+                        data = fp.read()
+                        # Turn the file contents into a pandas data frame
+                        df = pd.DataFrame(data)
+                        # Append data to master data frame
+                        master_sequence_alleler = master_sequence_alleler.append(df)
+                else:
+                    print(f'Skipping the following file because it cannot be found: {my_file}')
+
+master_sequence_alleler.to_csv(f'{output_file_path}/master_sequence_alleler.csv')
+
+# Convert CSV to TSV
+with open(f'{output_file_path}/master_sequence_alleler.csv', 'r') as csvin, open(f'{output_file_path}/master_sequence_alleler.tsv') as tsvout:
+    csvin = csv.reader(csvin)
+    tsvout = csv.writer(tsvout, delimiter = '\t')
+    
+    for row in csvin:
+        tsvout.writerow(row)
