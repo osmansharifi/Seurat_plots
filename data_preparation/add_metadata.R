6b855fbafa535a73e390d0b18c34175d28a88470
Osman <osharifi@ucdavis.edu>
2022-04-07 15:23:39 -0700

    Update add_metadata.R

--- data_preparation/add_metadata.R
+++ data_preparation/add_metadata.R
@@ -4,28 +4,28 @@ library(scCustomize)
 library(patchwork)
 
 #Load data
-load("/Users/osman/Desktop/LaSalle_lab/Seurat_objects/all.rett.combined.RData")
+load("/Users/osman/Desktop/LaSalle_lab/Seurat_objects/all.cortex.combined.RData")
 counts_table <- read.table("/Users/osman/Documents/GitHub/snRNA-seq-pipeline/data_preparation/master_sequence_alleler.txt", sep="\t", header=FALSE)
 #headers for the txt file
 names(counts_table) <- c("Barcode", "UMI", "WT", "MUT", "Body")
 
 sample_names <- c("WT_M_E18_WB1", "WT_M_E18_WB2", "MUT_M_E18_WB1", "MUT_M_E18_WB2", "WT_M_P30_CORT1", "WT_M_P30_CORT2","MUT_M_P30_CORT1", "MUT_M_P30_CORT2", "WT_M_P60_CORT1", "WT_M_P60_CORT2", "MUT_M_P60_CORT1", "MUT_M_P60_CORT2", "WT_M_P120_CORT1", "WT_M_P120_CORT2", "MUT_M_P120_CORT1","MUT_M_P120_CORT2", "MUT_F_P30_CORT1", "MUT_F_P30_CORT2", "MUT_F_P60_CORT1", "MUT_F_P60_CORT2", "MUT_F_P150_CORT1", "MUT_F_P150_CORT2", "MUT_F_P150_CORT3", "MUT_F_P150_CORT4", "WT_F_P30_CORT1", "WT_F_P30_CORT2", "WT_F_P60_CORT1", "WT_F_P60_CORT2", "WT_F_P150_CORT1", "WT_F_P150_CORT2", "WT_F_P150_CORT3", "WT_F_P150_CORT4", "MUT_F_E18_WB1", "MUT_F_E18_WB2", "WT_F_E18_WB1", "WT_F_E18_WB2")
 #parse by sex
-all.rett.combined$Sex <- plyr::mapvalues(
-  x = all.rett.combined$orig.ident, 
+all.cortex.combined$Sex <- plyr::mapvalues(
+  x = all.cortex.combined$orig.ident, 
   from = c(sample_names), 
   to = c("Male", "Male", "Male","Male","Male","Male","Male","Male","Male","Male","Male","Male","Male","Male","Male","Male","Female","Female","Female","Female","Female","Female","Female","Female","Female","Female","Female","Female","Female","Female","Female","Female","Female","Female","Female","Female")
 )
 #parse by condition
-all.rett.combined$Condition <- plyr::mapvalues(
-  x = all.rett.combined$orig.ident, 
+all.cortex.combined$Condition <- plyr::mapvalues(
+  x = all.cortex.combined$orig.ident, 
   from = c(sample_names), 
   to = c("WT", "WT", "MUTANT", "MUTANT", "WT", "WT", "MUTANT", "MUTANT","WT", "WT", "MUTANT", "MUTANT","WT", "WT", "MUTANT", "MUTANT","MUTANT","MUTANT","MUTANT","MUTANT","MUTANT","MUTANT","MUTANT","MUTANT","WT","WT","WT","WT","WT","WT","WT","WT","MUTANT", "MUTANT", "WT","WT")
 )
 
 #parse by Age
-all.rett.combined$Age <- plyr::mapvalues(
-  x = all.rett.combined$orig.ident, 
+all.cortex.combined$Age <- plyr::mapvalues(
+  x = all.cortex.combined$orig.ident, 
   from = c(sample_names), 
   to = c("E18", "E18","E18","E18","P30","P30","P30","P30","P60","P60","P60","P60","P120","P120","P120","P120","P30", "P30", "P60", "P60", "P150", "P150","P150", "P150","P30", "P30", "P60", "P60", "P150", "P150","P150", "P150","E18", "E18","E18","E18")
 )
@@ -59,7 +59,7 @@ Mecp2_wt_mut_counts$MUT = new.counts.mut
 is.unique(Mecp2_wt_mut_counts$Barcode)
 
 # making a data frame with just one vector representing all barcodes present in Seurat object by removing everything after the - symbol in the barcode names in the Seurat object
-dataFrame = data.frame(Barcode = sub("-.*", "", names(Idents(all.rett.combined))))
+dataFrame = data.frame(Barcode = sub("-.*", "", names(Idents(all.cortex.combined))))
 
 # adding in an id column to make sure we can sort the data back to the original order found in the Seurat Object
 dataFrame$id = 1:nrow(dataFrame)
@@ -77,7 +77,7 @@ merged = merged[order(merged$id),]
 merged[is.na(merged)] <- 0
 
 # Number of barcodes that are not in Seurat object
-number = nrow(merged) - all.rett.combined@assays$RNA@counts@Dim[2] # x number of Barcodes in the Mecp2 WT vs. MUT counts file did not have corresponding Barcodes in the Seurat Object
+number = nrow(merged) - all.cortex.combined@assays$RNA@counts@Dim[2] # x number of Barcodes in the Mecp2 WT vs. MUT counts file did not have corresponding Barcodes in the Seurat Object
 
 # Removing rows with barcodes that are not in Seurat object
 merged = merged[c(1:(nrow(merged)-number)),]
@@ -86,31 +86,31 @@ nrow(merged) # should equal length of all metadata
 
 ### Adding counts to Seurat metadata 
 
-all.rett.combined$WT_Mecp2 = merged$WT
+all.cortex.combined$WT_Mecp2 = merged$WT
 
-all.rett.combined$MUT_Mecp2 = merged$MUT
+all.cortex.combined$MUT_Mecp2 = merged$MUT
 
 ### Adding counts to Seurat counts as two new genes
 
-all.rett.combined@assays$RNA@counts = rbind(all.rett.combined@assays$RNA@counts, merged$WT, merged$MUT)
-nrow(all.rett.combined@assays$RNA@counts)
-rownames(all.rett.combined@assays$RNA@counts) = c(rownames(all.rett.combined@assays$RNA@counts)[-c(19665:19666)], "Mecp2-WT", "Mecp2-MUT")
+all.cortex.combined@assays$RNA@counts = rbind(all.cortex.combined@assays$RNA@counts, merged$WT, merged$MUT)
+nrow(all.cortex.combined@assays$RNA@counts)
+rownames(all.cortex.combined@assays$RNA@counts) = c(rownames(all.cortex.combined@assays$RNA@counts)[-c(19665:19666)], "Mecp2-WT", "Mecp2-MUT")
 
 # checking to make sure they are added
-all.rett.combined@assays$RNA@counts[19665:19666, 1:5]
+all.cortex.combined@assays$RNA@counts[19665:19666, 1:5]
 
-#E18 <- subset(x = all.rett.combined, subset = orig.ident == c("MUT_F_E18_WB1", "MUT_F_E18_WB2", "WT_F_E18_WB1", "WT_F_E18_WB2"))
-all.rett.combined <- NormalizeData(all.rett.combined)
-all.rett.combined <- ScaleData(all.rett.combined)
-all.rett.combined <- FindVariableFeatures(all.rett.combined, selection.method = "vst", nfeatures = 3000)
-all.rett.combined <- RunPCA(object = all.rett.combined, verbose = FALSE)
-all.rett.combined <- RunUMAP(object = all.rett.combined, dims = 1:20, verbose = FALSE)
-all.rett.combined <- FindNeighbors(object = all.rett.combined, dims = 1:20, verbose = FALSE)
-all.rett.combined <- FindClusters(object = all.rett.combined, verbose = FALSE)
-DimPlot(object = all.rett.combined, label = TRUE, group.by = 'celltype.call') + NoLegend() + ggtitle("sctransform")# saving new Seurat object
-FeaturePlot_scCustom(seurat_object = all.rett.combined, features = 'WT_Mecp2', split.by = "Sex")
-FeaturePlot_scCustom(seurat_object = all.rett.combined, features = 'MUT_Mecp2', split.by = "Sex")
-FeaturePlot_scCustom(seurat_object = all.rett.combined, features = 'Mecp2', split.by = "Sex")
+#E18 <- subset(x = all.cortex.combined, subset = orig.ident == c("MUT_F_E18_WB1", "MUT_F_E18_WB2", "WT_F_E18_WB1", "WT_F_E18_WB2"))
+all.cortex.combined <- NormalizeData(all.cortex.combined)
+all.cortex.combined <- ScaleData(all.cortex.combined)
+all.cortex.combined <- FindVariableFeatures(all.cortex.combined, selection.method = "vst", nfeatures = 3000)
+all.cortex.combined <- RunPCA(object = all.cortex.combined, verbose = FALSE)
+all.cortex.combined <- RunUMAP(object = all.cortex.combined, dims = 1:20, verbose = FALSE)
+all.cortex.combined <- FindNeighbors(object = all.cortex.combined, dims = 1:20, verbose = FALSE)
+all.cortex.combined <- FindClusters(object = all.cortex.combined, verbose = FALSE)
+DimPlot(object = all.cortex.combined, label = TRUE, group.by = 'celltype.call') + NoLegend() + ggtitle("sctransform")# saving new Seurat object
+FeaturePlot_scCustom(seurat_object = all.cortex.combined, features = 'WT_Mecp2', split.by = "Sex")
+FeaturePlot_scCustom(seurat_object = all.cortex.combined, features = 'MUT_Mecp2', split.by = "Sex")
+FeaturePlot_scCustom(seurat_object = all.cortex.combined, features = 'Mecp2', split.by = "Sex")
 
 #Function counts the percent of total cells that express specific genes
 PrctCellExpringGene <- function(object, genes, group.by = "all"){
@@ -141,7 +141,7 @@ calc_helper <- function(object,genes){
   }else{return(NA)}
 }
 
-PrctCellExpringGene(all.rett.combined, genes =c("WT-Mecp2","MUT-Mecp2"), group.by = "all")
+PrctCellExpringGene(all.cortex.combined, genes =c("WT-Mecp2","MUT-Mecp2"), group.by = "all")
 
 GOI1 <- 'WT_Mecp2' #you will have to name your first gene here, im choosing PDX1 as an example
 GOI2 <- 'MUT_Mecp2' #you will have to name your second gene here, im choosing INS as an example
@@ -161,7 +161,7 @@ GOI1_GOI2.cells/all.cells.incluster*100 #Percentage of cells in Beta that co-exp
 cell_markers_manual <- c("Plch2","Sst","Vip", "Pvalb", "Slc17a8", "Macc1", "Rorb", "Fezf2", "Rprm", "Aqp4", "Rassf10", "Kcnj8", "Slc17a7", "Gad2", "Aspa")
 
 markers_df <- FindAllMarkers(
-  object = all.rett.combined, 
+  object = all.cortex.combined, 
   only.pos = TRUE, 
   min.pct = 0.25, 
   thresh.use = 0.25
@@ -170,11 +170,11 @@ markers_all_single <- markers_df[markers_df$gene %in% names(table(markers_df$gen
 top5 <- markers_all_single %>% group_by(cluster) %>% top_n(5, avg_log2FC)
 dim(top5)
 DoHeatmap(
-  object = all.rett.combined, 
+  object = all.cortex.combined, 
   features = top5$gene,
   labels = FALSE) 
 
 
 
-save(all.rett.combined, file="/Users/osman/Desktop/LaSalle_lab/Seurat_objects/all.rett.combined.RData")
-save(all.rett.combined, file=glue::glue("/Users/karineier/Documents/GitHub/snRNA-seq-pipeline/Parsing_Mecp2_trnx_expression/{all.rett.combined.name}/{all.rett.combined.name}_with_Mecp2_WT_MUT.RData"))
+save(all.cortex.combined, file="/Users/osman/Desktop/LaSalle_lab/Seurat_objects/all.cortex.combined.RData")
+save(all.cortex.combined, file=glue::glue("/Users/karineier/Documents/GitHub/snRNA-seq-pipeline/Parsing_Mecp2_trnx_expression/{all.cortex.combined.name}/{all.cortex.combined.name}_with_Mecp2_WT_MUT.RData"))
