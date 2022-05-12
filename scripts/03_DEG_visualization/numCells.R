0704a2409b57f6b1f4a2a5719904a60bcbfecd4a
Osman <osharifi@ucdavis.edu>
2022-05-12 11:34:46 -0700

    load data in

--- scripts/03_DEG_visualization/numCells.R
+++ scripts/03_DEG_visualization/numCells.R
@@ -4,10 +4,12 @@ library(dplyr)
 library(ggplot2)
 library(plyr)
 
-#Custom color palette
+# Custom color palette
 polychrome_palette  <- c("#5A5156FF","#E4E1E3FF","#F6222EFF","#FE00FAFF","#16FF32FF","#3283FEFF","#FEAF16FF","#B00068FF","#1CFFCEFF","#90AD1CFF","#2ED9FFFF","#DEA0FDFF","#AA0DFEFF","#F8A19FFF","#325A9BFF","#C4451CFF","#1C8356FF","#85660DFF","#B10DA1FF","#FBE426FF","#1CBE4FFF","#FA0087FF","#FC1CBFFF","#F7E1A0FF","#C075A6FF","#782AB6FF","#AAF400FF","#BDCDFFFF","#822E1CFF","#B5EFB5FF","#7ED7D1FF","#1C7F93FF","#D85FF7FF","#683B79FF","#66B0FFFF", "#3B00FBFF")
 # Order that celltypes should appear in 
 x = c("L2_3_IT", "L4", "L5", "L6","Pvalb", "Vip", "Sst","Sncg","Lamp5","Peri", "Endo", "Oligo","Astro","Non-neuronal")
+# Load data
+load("/Users/osman/Desktop/LaSalle_lab/Seurat_objects/all.cortex.combined.RData")
 meta.data <- all.cortex.combined[[]]
 
 # create random classifications for the sake of this example
@@ -24,6 +26,7 @@ counts_postnatal_wt_male <- filter(counts, Sex =="Male", Age!=18, Condition=="WT
 counts_postnatal_mut_male <- filter(counts, Sex =="Male", Age!=18, Condition=="MUTANT")
 counts_postnatal_wt_female <- filter(counts, Sex =="Female", Age!=18, Condition=="WT")
 counts_postnatal_mut_female <- filter(counts, Sex =="Female", Age!=18, Condition=="MUTANT")
+counts_E18 <- filter(counts, Age!= c(30, 60, 120, 150))
 ##plot the line graphs
 wt_m_plot <- ggplot(counts_postnatal_wt_male, aes(Age, count, color = cell_type), group = 3) +
   geom_line()+
