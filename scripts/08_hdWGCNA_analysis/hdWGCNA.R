41c25bcdfd9ab0716f696a0b9a2a00a59aeb4d01
Osman <osharifi@ucdavis.edu>
2023-02-07 13:31:17 -0800

    Update hdWGCNA.R

--- scripts/08_hdWGCNA_analysis/hdWGCNA.R
+++ scripts/08_hdWGCNA_analysis/hdWGCNA.R
@@ -18,7 +18,7 @@ theme_set(theme_cowplot())
 set.seed(1234)
 
 # load the snRNA-seq dataset
-load("/Users/osman/Desktop/LaSalle_lab/adult_maleects/all_male_mouse_cortex.RData")
+load("/Users/osman/Desktop/LaSalle_lab/Seurat_objects/all_male_mouse_cortex.RData")
 
 # Prepare Seurat Object for WGCNA
 metadata <- all_male@meta.data
@@ -28,11 +28,11 @@ timepoint <- lapply(metadata$orig.ident, function(x) {
 })
 all_male@meta.data$Time_Point <- unlist(timepoint)
 
-condition <- lapply(metadata$orig.ident, function(x) {
+genotype <- lapply(metadata$orig.ident, function(x) {
   split_name <- strsplit(x, "_")[[1]]
   return(split_name[1])
 })
-all_male@meta.data$Condition <- unlist(condition)
+all_male@meta.data$genotype <- unlist(genotype)
 
 # Subset on a value in the object meta data
 adult_male <- subset(x = all_male, subset = Time_Point != "E18")
@@ -45,7 +45,7 @@ adult_male <- FindVariableFeatures(adult_male, selection.method = "vst", nfeatur
 adult_male <- RunPCA(adult_male, features = VariableFeatures(object = adult_male))
 adult_male <- RunUMAP(adult_male, dims = 1:20)
 DimPlot(adult_male, group.by='celltype.call', label=TRUE) +
-  umap_theme() + NoLegend()
+  umap_theme() 
 
 # Set up Seurat object for WGCNA
 adult_male <- SetupForWGCNA(
@@ -57,8 +57,8 @@ adult_male <- SetupForWGCNA(
 
 # construct metacells  in each group
 adult_male <- MetacellsByGroups(
-  adult_male = adult_male,
-  group.by = c("celltype.call", "Time_Point", "Condition"), # specify the columns in adult_male@meta.data to group by
+  adult_male,
+  group.by = c("celltype.call", "Time_Point"), # specify the columns in adult_male@meta.data to group by
   k = 25, # nearest-neighbors parameter
   max_shared = 10, # maximum number of shared cells between two metacells
   ident.group = 'celltype.call' # set the Idents of the metacell seurat object
@@ -93,10 +93,10 @@ wrap_plots(plot_list, ncol=2)
 adult_male <- ConstructNetwork(
   adult_male, soft_power=5,
   setDatExpr=FALSE,
-  tom_name = 'P30' # name of the topoligical overlap matrix written to disk
+  overwrite_tom = TRUE# name of the topoligical overlap matrix written to disk
 )
 
-PlotDendrogram(adult_male, main='P30 hdWGCNA Dendrogram')
+PlotDendrogram(adult_male, main='hdWGCNA Dendrogram')
 
 # need to run ScaleData first or else harmony throws an error:
 adult_male <- ScaleData(adult_male, features=VariableFeatures(adult_male))
@@ -149,7 +149,7 @@ plot_list <- ModuleFeaturePlot(
 )
 
 # stitch together with patchwork
-wrap_plots(plot_list, ncol=6)
+wrap_plots(plot_list, ncol=3)
 
 # make a featureplot of hub scores for each module
 plot_list <- ModuleFeaturePlot(
@@ -160,7 +160,7 @@ plot_list <- ModuleFeaturePlot(
 
 
 # stitch together with patchwork
-wrap_plots(plot_list, ncol=6)
+wrap_plots(plot_list, ncol=3)
 
 # plot module correlagram
 ModuleCorrelogram(adult_male)
@@ -185,7 +185,7 @@ p
 
 ## Compute Correlations
 # convert genotype to factor
-adult_male$Condition <- as.factor(adult_male$Condition)
+adult_male$Genotype <- as.factor(adult_male$Genotype)
 # convert time point to factor
 adult_male$Time_Point <- as.factor(adult_male$Time_Point)
 # convert celltype to factor
@@ -194,7 +194,7 @@ adult_male$celltype.call <- as.factor(adult_male$celltype.call)
 adult_male$orig.ident <- as.factor(adult_male$orig.ident)
 
 # list of traits to correlate
-cur_traits <- c('Condition', 'Time_Point', 'orig.ident')
+cur_traits <- c('Genotype', 'Time_Point')
 
 adult_male <- ModuleTraitCorrelation(
   adult_male,
@@ -219,3 +219,18 @@ PlotModuleTraitCorrelation(
   plot_max = 0.1,
   combine=TRUE
 )
+
+moduleDendro <- getDendro(MEs, distance = "bicor")
+plotDendro(moduleDendro, labelSize = 4, nBreaks = 5)
+
+moduleCor <- getCor(MEs, corType = "bicor")
+plotHeatmap(moduleCor, rowDendro = moduleDendro, colDendro = moduleDendro)
+moduleCorStats <- getMEtraitCor(MEs, colData = MEs, corType = "bicor", robustY = TRUE)
+
+MEtraitCor <- getMEtraitCor(MEs, colData = colData, corType = "bicor")
+traitDendro <- getCor(MEs, y = colData, corType = "bicor", robustY = FALSE) %>% getDendro(transpose = TRUE)
+plotDendro(traitDendro, labelSize = 3.5, expandY = c(0.65,0.08), file = "Trait_Dendrogram.pdf")
+
+plotMEtraitCor(mt_cor, moduleOrder = moduleDendro$order, traitOrder = traitDendro$order, topOnly = TRUE, label.type = "p", label.size = 4, label.nudge_y = 0, legend.position = c(1.11, 0.795), colColorMargins = c(-1,4.75,0.5,10.1), file = "Top_ME_Trait_Correlation_Heatmap.pdf", width = 8.5, height = 4.25)
+
+plotMEtraitCor(mt_cor, moduleOrder = moduleDendro$order, traitOrder = traitDendro$order)
