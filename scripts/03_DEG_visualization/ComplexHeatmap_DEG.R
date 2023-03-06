cb9452e6c1b8faf9396d38c70569d876cc1124b3
Osman <osharifi@ucdavis.edu>
2023-03-05 18:17:44 -0800

    complexheatmap

--- scripts/03_DEG_visualization/ComplexHeatmap_DEG.R
+++ scripts/03_DEG_visualization/ComplexHeatmap_DEG.R
@@ -1,33 +1,67 @@
-#Integrate all data frames together to create a DEG heatmap
-data.frame.list = list(L2_3_IT_DEG, L4_DEG, L5_DEG, L6_DEG, Sst_DEG, Pvalb_DEG, Vip_DEG, Oligo_DEG, Lamp5_DEG, Nonneuronal_DEG)
-#gene.set = unique(rownames(Reduce(rbind, data.frame.list)))
-gene.set = unique(unlist(lapply(data.frame.list, rownames)))
-ct.set   = unique(colnames(Reduce(cbind, data.frame.list)))
-DEG.matrix = matrix(0, nrow=length(gene.set), ncol=length(ct.set), dimnames=list(gene.set, ct.set))
-for(df in data.frame.list){
-  DEG.matrix[rownames(df), colnames(df)] = df[,1] ## Not sure what here, pvalue? foldchange? or binary existence
-}
-DEG.df = as.data.frame(DEG.matrix)
+###################
+##Load Libraries ##
+###################
 library(ComplexHeatmap)
-## Draws a heatmap with rows and columns seperated by a predefined label using ComplexHeatmap
-plotDEG = function(data,       ## Data matrix
-                   row.labels, ## Row annotation for data, assumed to be a vector of characters
-                   col.labels, ## Column annotation for data, assumed to be a vector of characters
-                   filename="~/P30_Male_cortex.png"){
-  column_anno = HeatmapAnnotation(celltype=as.character(col.labels))
-  ## Plot proportion heatmap
-  DEG.heatmap = Heatmap(data,
-                    top_annotation=column_anno,
-                    col = c('purple', 'yellow'),
-                    cluster_rows = T,
-                    cluster_columns = T,
-                    show_row_names = T,
-                    row_split=factor(row.labels),
-                    column_split=factor(col.labels),
-                    show_heatmap_legend=T,
-                    border=T)
-  ## Plot
-  png(filename, width=12, height=12, units='in', res=300)
-  draw(DEG.heatmap)
-  dev.off()
-}
+library(circlize)
+library(dplyr)
+library(tidyr)
+
+##########################
+##Load and prepare data ##
+##########################
+male_matrix <- read.csv("/Users/osman/Documents/GitHub/snRNA-seq-pipeline/DEG_data/LimmaVoomCC/male_mouse_logfc.csv")
+rownames(male_matrix) = male_matrix$SYMBOL
+male_matrix$SYMBOL <- NULL
+male_matrix$X <- NULL
+male_matrix = as.matrix(male_matrix)
+pv_male <- read.csv("/Users/osman/Documents/GitHub/snRNA-seq-pipeline/DEG_data/LimmaVoomCC/male_mouse_pv.csv")
+rownames(pv_male) = pv_male$SYMBOL
+pv_male$SYMBOL <- NULL
+pv_male$X <- NULL
+pv_male = as.matrix(pv_male)
+male_metadata <- read.csv("/Users/osman/Documents/GitHub/snRNA-seq-pipeline/DEG_data/LimmaVoomCC/male_mouse_meta.csv")
+
+####################
+## Create Heatmap ##
+####################
+
+#Set color palette
+polychrome_palette <- c("#5A5156FF","#E4E1E3FF","#F6222EFF","#FE00FAFF","#16FF32FF","#3283FEFF","#FEAF16FF","#B00068FF","#1CFFCEFF","#90AD1CFF","#2ED9FFFF","#DEA0FDFF","#AA0DFEFF","#F8A19FFF","#325A9BFF","#C4451CFF","#1C8356FF","#85660DFF","#B10DA1FF","#FBE426FF","#1CBE4FFF","#FA0087FF","#FC1CBFFF","#F7E1A0FF","#C075A6FF","#782AB6FF","#AAF400FF","#BDCDFFFF","#822E1CFF","#B5EFB5FF","#7ED7D1FF","#1C7F93FF","#D85FF7FF","#683B79FF","#66B0FFFF", "#3B00FBFF")
+col_fun = colorRamp2(c(min(male_matrix), 0.0, max(male_matrix)), c("#2166AC", "#EEEEEE", "#B2182B")) 
+
+# Set column and row annotations
+
+row_ha = rowAnnotation(Genes = rownames(male_matrix))
+column_ha = HeatmapAnnotation(`Cell Type` = male_metadata$Cell.Type, 
+                              `Time Point` = male_metadata$Time.Point,
+                              Sex = male_metadata$Sex,
+                              col = list(`Time Point` = c("P30" = "#E6B8BFFF","P60"= "#CC7A88FF","P120"= "#B33E52FF", "P150" = "#990F26FF"), 
+                                         Sex = c("Male" = "#0F8299FF", "Female" = "#3D0F99FF"),
+                                         `Cell Type` = c("L2_3_IT" = polychrome_palette[1], "L4" = polychrome_palette[2], "L5"= polychrome_palette[3], "Pvalb"= polychrome_palette[4],"Vip"= polychrome_palette[5], "Sst"= polychrome_palette[6],"Sncg"= polychrome_palette[7], "Lamp5"= polychrome_palette[8], "Peri"= polychrome_palette[9], "Astro"= polychrome_palette[13])),
+                              annotation_name_gp = gpar(fontsize = 14, fontface = 'bold'))
+
+# Create heatmap
+
+pdf(file="Top Postnatal Male DEGs.pdf", height = 15, width = 16)
+map = grid.grabExpr(
+  draw(
+    Heatmap(male_matrix, 
+            name = "logFC", 
+            top_annotation = column_ha, 
+            col = col_fun, 
+            row_names_gp=gpar(fontsize=14),
+            column_names_gp=gpar(fontsize=14),
+            cluster_columns = FALSE, 
+            column_title = "Top Postnatal Male DEGs", 
+            heatmap_legend_param = list(title="logFC", 
+                                        title_gp = gpar(fontsize = 14, fontface = 'bold'), 
+                                        labels_gp = gpar(fontsize = 12, fontface = 'bold')), 
+            cell_fun = function(j, i, x, y, width, height, fill) {
+  if( pv_male[i, j] <= 0.05 ) {
+    grid.text(print("*"), x, y-height/3, gp = gpar(fontsize=14)) #grid.text(print("*"), x, y, gp = gpar(fontsize=9)) 
+  }
+})))
+grid.newpage()
+grid.draw(map)
+popViewport()
+dev.off()
