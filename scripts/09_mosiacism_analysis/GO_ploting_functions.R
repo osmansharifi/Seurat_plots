4d46a46f46d03eea26f3b2e99390e4c9c0a6ebd0
Osman <osharifi@ucdavis.edu>
2023-07-19 16:45:12 -0700

    mosaic data

---
+++ scripts/09_mosiacism_analysis/GO_ploting_functions.R
@@ -0,0 +1,155 @@
+slimGO <- function(GO = GO,
+                   tool = c("enrichR", "rGREAT", "GOfuncR"),
+                   annoDb = annoDb,
+                   plots = FALSE,
+                   threshold = 0.7){
+  
+  if(tool == "enrichR"){
+    GO <- GO %>%
+      data.table::rbindlist(idcol = "Gene Ontology") %>%
+      dplyr::as_tibble() %>%
+      dplyr::filter(`Gene Ontology` %in% c("GO_Biological_Process_2018",
+                                           "GO_Cellular_Component_2018",
+                                           "GO_Molecular_Function_2018")) %>% 
+      dplyr::mutate(Term = stringr::str_extract(.$Term, "\\(GO.*")) %>%
+      dplyr::mutate(Term = stringr::str_replace_all(.$Term, "[//(//)]",""), "") %>%
+      dplyr::mutate("Gene Ontology" = dplyr::case_when(`Gene Ontology` == "GO_Biological_Process_2018" ~ "BP",
+                                                       `Gene Ontology` == "GO_Cellular_Component_2018" ~ "CC",
+                                                       `Gene Ontology` == "GO_Molecular_Function_2018" ~ "MF")) %>%
+      dplyr::select(p = P.value, go = Term, "Gene Ontology") %>% 
+      dplyr::filter(p < 0.05)
+    
+  }else if(tool == "rGREAT"){
+    
+    GO <-  GO %>%
+      data.table::rbindlist(idcol = "Gene Ontology") %>%
+      dplyr::as_tibble() %>%
+      dplyr::mutate("Gene Ontology" = dplyr::case_when(`Gene Ontology` == "GO Biological Process" ~ "BP",
+                                                       `Gene Ontology` == "GO Cellular Component" ~ "CC",
+                                                       `Gene Ontology` == "GO Molecular Function" ~ "MF")) %>%
+      dplyr::select(p = Hyper_Raw_PValue, go = ID, "Gene Ontology") %>% 
+      dplyr::filter(p < 0.05)
+    
+  }else if(tool == "GOfuncR"){
+    
+    GO <- GO$results %>%
+      dplyr::as_tibble() %>%
+      dplyr::mutate("Gene Ontology" = dplyr::case_when(ontology == "biological_process" ~ "BP",
+                                                       ontology == "cellular_component" ~ "CC",
+                                                       ontology == "molecular_function" ~ "MF")) %>%
+      dplyr::select(p = raw_p_overrep, go = node_id, "Gene Ontology") %>% 
+      dplyr::filter(p < 0.05)
+    
+  }else{
+    stop(glue("{tool} is not supported, please choose either enrichR, rGREAT, or GOfuncR [Case Sensitive]"))
+  }
+  
+  print(glue::glue("Submiting results from {tool} to rrvgo..."))
+  
+  .slim <- function(GO = GO,
+                    ont = ont,
+                    annoDb = annoDb,
+                    plots = plots,
+                    tool = tool,
+                    threshold = threshold){
+    GO <- GO %>%
+      dplyr::filter(`Gene Ontology` == ont)
+    
+    print(glue::glue("rrvgo is now slimming {ont} GO terms from {tool}"))
+    
+    simMatrix  <- rrvgo::calculateSimMatrix(GO$go,
+                                            orgdb = annoDb,
+                                            ont = ont,
+                                            method = "Rel")
+    
+    reducedTerms <- rrvgo::reduceSimMatrix(simMatrix,
+                                           setNames(-log10(GO$p), GO$go),
+                                           threshold = threshold,
+                                           orgdb = annoDb) 
+    
+    if(plots == TRUE){
+      p <- rrvgo::scatterPlot(simMatrix, reducedTerms) # Doesn't plot otherwise
+      plot(p) 
+      rrvgo::treemapPlot(reducedTerms)
+    }
+    
+    print(glue::glue("There are {max(reducedTerms$cluster)} clusters in your GO {ont} terms from {tool}"))
+    
+    reducedTerms %>%   
+      dplyr::as_tibble() %>%
+      return()
+  }
+  
+  slimmed <- GO %>%
+    dplyr::select(`Gene Ontology`) %>%
+    table() %>%
+    names() %>% 
+    purrr::set_names() %>%
+    purrr::map_dfr(~.slim(GO = GO,
+                          ont = .,
+                          annoDb = annoDb,
+                          tool = tool,
+                          plots = plots,
+                          threshold = threshold),
+                   .id = "Gene Ontology") %>%
+    dplyr::inner_join(GO) %>%
+    dplyr::filter(term == as.character(parentTerm)) %>%
+    dplyr::mutate("-log10.p-value" = -log10(p)) %>%
+    dplyr::mutate("Gene Ontology" = dplyr::recode_factor(`Gene Ontology`,
+                                                         "BP" = "Biological Process",
+                                                         "CC" = "Cellular Component",
+                                                         "MF" = "Molecular Function")) %>%
+    dplyr::arrange(dplyr::desc(`-log10.p-value`)) %>% 
+    dplyr::select("Gene Ontology", Term = term, "-log10.p-value") %>% 
+    return()
+}
+
+#' GOplot
+#' @title Plot slimmed GO results
+#' @description Plots top significant slimmed Gene Ontology terms from from \code{enrichR},
+#'  \code{rGREAT}, and \code{GOfuncR}.
+#' @param slimmedGO A \code{tibble} from \code{DMRichR::rrvgo()} or \code{DMRichR::REVIGO()}.
+#' @return A \code{ggplot} object of top significant GO and pathway terms from an \code{enrichR}, 
+#' \code{GOfuncR}, or \code{rGREAT} analysis that can be viewed by calling it, saved with 
+#' \code{ggplot2::ggsave()}, or further modified by adding \code{ggplot2} syntax.
+#' @import ggplot2
+#' @importFrom magrittr %>%
+#' @importFrom dplyr mutate select group_by slice ungroup
+#' @importFrom forcats fct_rev
+#' @importFrom ggsci scale_fill_d3
+#' @importFrom Hmisc capitalize
+#' @importFrom stringr str_trim str_trunc
+#' @importFrom glue glue
+#' @export GOplot
+#' 
+GOplot <- function(slimmedGO = slimmedGO){
+  
+  slimmedGO %>% 
+    dplyr::group_by(`Gene Ontology`) %>%
+    dplyr::slice(1:7) %>%
+    dplyr::ungroup() %>% 
+    dplyr::mutate(Term = stringr::str_trim(.$Term)) %>%
+    dplyr::mutate(Term = Hmisc::capitalize(.$Term)) %>%
+    dplyr::mutate(Term = stringr::str_wrap(.$Term, 50)) %>% 
+    #dplyr::mutate(Term = stringr::str_trunc(.$Term, 45, side = "right")) %>% 
+    dplyr::mutate(Term = factor(.$Term, levels = unique(.$Term[order(forcats::fct_rev(.$`Gene Ontology`), .$`-log10.p-value`)]))) %>% 
+    ggplot2::ggplot(ggplot2::aes(x = Term,
+                                 y = `-log10.p-value`,
+                                 fill = `Gene Ontology`,
+                                 group = `Gene Ontology`)) +
+    ggplot2::geom_bar(stat = "identity",
+                      position = ggplot2::position_dodge(),
+                      color = "Black") +
+    ggplot2::coord_flip() +
+    ggplot2::scale_y_continuous(expand = c(0, 0, 0.1, 0)) +
+    ggsci::scale_fill_d3() +
+    ggplot2::labs(y = expression("-log"[10](p))) +
+    ggplot2::theme_classic() +
+    ggplot2::theme(axis.text = ggplot2::element_text(size = 14),
+                   axis.title.y = ggplot2::element_blank(),
+                   legend.text = ggplot2::element_text(size = 14),
+                   legend.title = ggplot2::element_text(size = 14),
+                   strip.text = ggplot2::element_text(size = 14)) %>% 
+    return()
+}
+
