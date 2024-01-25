0dcffabc28b54adf2451b2e157366401c35a8d82
Osman <osharifi@ucdavis.edu>
2024-01-25 14:46:49 -0800

    upset kegg

--- scripts/09_mosiacism_analysis/upset_kegg.R
+++ scripts/09_mosiacism_analysis/upset_kegg.R
@@ -1,94 +1,81 @@
-# create a common kegg term plot between different timepoints of mouse WT vs WT
+##############################
+## Upset plot of KEGG terms ##
+##############################
+# Author : Osman Sharifi
 
+# Load libraries
+library(UpSetR)
 library(dplyr)
 library(ggplot2)
-base_path <- '/Users/osman/Documents/GitHub/snRNA-seq-pipeline/scripts/09_mosiacism_analysis/'
+library(glue)
+
 # Load data
-kegg_terms <- readxl::read_xlsx(glue::glue("{base_path}broad_group_analysis/GABA_kegg_all.xlsx",col_names = TRUE))
+read_and_filter_kegg <- function(file_path) {
+  kegg_terms <- readxl::read_xlsx(file_path, col_names = TRUE)
+  kegg_terms <- filter(kegg_terms, P.value <= 0.05)
+  
+  # Convert column name to "Time_Point" (case-insensitive)
+  col_name <- tolower("Time_Point")
+  if (col_name %in% names(kegg_terms)) {
+    kegg_terms$Time_Point <- kegg_terms[[col_name]]
+    kegg_terms <- select(kegg_terms, -col_name)
+  }
+  
+  return(kegg_terms)
+}
+base_path <- '/Users/osman/Documents/GitHub/snRNA-seq-pipeline/scripts/09_mosiacism_analysis/'
 
-kegg_terms <- filter(kegg_terms, P.value <= 0.05)
-kegg_terms$Time_Point = kegg_terms$Time_Point
-# Get the top 5 terms for each cell type
-top_terms <- kegg_terms %>%
-  group_by(Time_Point) %>%
-  top_n(10, P.value) 
+# Read and filter GABA kegg terms
+gaba_kegg_terms_path <- glue::glue("{base_path}broad_group_analysis/GABA_kegg_all.xlsx")
+expr3_gaba <- read_and_filter_kegg(gaba_kegg_terms_path)
 
-top_terms$Time_Point <- factor(top_terms$Time_Point, levels = c("P30", "P60", "P150"))
-ggplot(top_terms, aes(x = Term, y = Time_Point, color = P.value, size = Odds.Ratio)) +
-  geom_point() +
-  scale_color_gradientn(name = "P-value", colors = RColorBrewer::brewer.pal(n = 11, name = "RdBu")) +
-  scale_size_continuous(range = c(3, 8)) +
-  scale_y_discrete(name = "Time point") +
-  scale_x_discrete(name = "Terms") +
-  theme_minimal() +
-  guides(shape = guide_legend(override.aes = list(size = 5))) +
-  labs(title = 'Top10 GABAergic KEGG terms') +
-  theme(legend.position = "bottom")+
-  theme_bw(base_size = 24) +
-  theme(
-    legend.position = 'right',
-    legend.background = element_rect(),
-    plot.title = element_text(angle = 0, size = 18, face = 'bold', vjust = 1),
-    plot.subtitle = element_text(angle = 0, size = 14, face = 'bold', vjust = 1),
-    plot.caption = element_text(angle = 0, size = 14, face = 'bold', vjust = 1),
-    
-    axis.text.x = element_text(angle = 90, size = 12, face = 'bold', hjust = 1.0, vjust = 0.5, colour = "black"),
-    axis.text.y = element_text(angle = 0, size = 8, face = 'plain', vjust = 0.5, colour = "black"),
-    axis.title = element_text(size = 18, face = 'bold', colour = "black"),
-    axis.title.x = element_text(size = 18, face = 'bold', colour = "black"),
-    axis.title.y = element_text(size = 18, face = 'bold', colour = "black"),
-    axis.line = element_line(colour = 'black'),
-    
-    #Legend
-    legend.key = element_blank(), # removes the border
-    legend.key.size = unit(1, "cm"), # Sets overall area/size of the legend
-    legend.text = element_text(size = 18, face = "bold"), # Text size
-    title = element_text(size = 18, face = "bold")) +
-  coord_flip()
-ggsave(glue::glue("{base_path}mut_from_mut_vs_wt_from_wt/GABA_top10_kegg.pdf"), 
-       device = NULL,
-       height = 8.5,
-       width = 7)
+# Read and filter Glut kegg terms
+glut_kegg_terms_path <- glue::glue("{base_path}broad_group_analysis/Glut_kegg_all.xlsx")
+expr3_glut <- read_and_filter_kegg(glut_kegg_terms_path)
 
-###########################################
-## Venn diagram of the overlapping genes ##
-###########################################
-library(VennDiagram)
-library(grDevices)
-# Extract significant DEGs
-sig_kegg_P30 <- rownames(deg_results$P30)[deg_results$P30$adj.P.Val < 0.05]
-sig_kegg_P60 <- rownames(deg_results$P60)[deg_results$P60$adj.P.Val < 0.05]
-sig_kegg_P150 <- rownames(deg_results$P150)[deg_results$P150$adj.P.Val < 0.05]
-intersection_all2 <- intersect(sig_genes_P150,sig_genes_P30)
-intersection_all3 <- intersect(intersection_all2,sig_genes_P60)
-# Extract KEGG terms
-p30_rows <- subset(kegg_terms, Time_Point == "P30")
-# Filter rows for P60
-p60_rows <- subset(kegg_terms, Time_Point == "P60")
-# Filter rows for P150
-p150_rows <- subset(kegg_terms, Time_Point == "P150")
-intersection_all2 <- intersect(p30_rows$Term,p60_rows$Term)
-intersection_all3 <- intersect(intersection_all2,p150_rows$Term)
-# Create a Venn diagram
-pdf(file=glue("{base_path}mut_from_mut_vs_wt_from_wt/venn_kegg_GABA.pdf"))
-temp<- venn.diagram(
-  x = list(
-    P30 = p30_rows$Term,
-    P60 = p60_rows$Term,
-    P150 = p150_rows$Term
-  ),
-  category.names = c("P30", "P60", "P150"),
-  main = 'GABA KEGG Terms of MUT cells from MUT females and WT cells from WT females ',
-  filename = NULL,
-  col = c("#E6B8BFFF","#CC7A88FF","#990F26FF"),
-  fill = c("#E6B8BFFF","#CC7A88FF","#990F26FF"),
-  cat.cex = 1.2,
-  cat.fontface = "bold",
-  euler.d = TRUE,
-  disable.logging = TRUE,
-  hyper.test = TRUE
-)
-grid.draw(temp)
-dev.off()
-# Write the merged dataframe to a CSV file
-write.csv(merged, file = "/Users/osman/Documents/GitHub/snRNA-seq-pipeline/KEGG_data/human_male_mouse_kegg_overlap.csv", row.names = FALSE)
+# Read and filter mut_from_mut_vs_wt_from_wt Glut kegg terms
+mut_glut_kegg_terms_path <- glue::glue("{base_path}mut_from_mut_vs_wt_from_wt/Glut_kegg_all.xlsx")
+expr4_glut <- read_and_filter_kegg(mut_glut_kegg_terms_path)
+
+# Read and filter mut_from_mut_vs_wt_from_wt GABA kegg terms
+mut_gaba_kegg_terms_path <- glue::glue("{base_path}mut_from_mut_vs_wt_from_wt/GABA_kegg_all.xlsx")
+expr4_gaba <- read_and_filter_kegg(mut_gaba_kegg_terms_path)
+
+#######################
+## Create upset plot ##
+#######################
+# Function to create a list from Time_Point and Term columns
+create_list_from_df <- function(df) {
+  time_points <- unique(df$Time_point)
+  result_list <- lapply(time_points, function(tp) {
+    terms <- df$Term[df$Time_point == tp]
+    return(setNames(terms, NULL))  # Exclude "Time_Point" and "Term" as names
+  })
+  names(result_list) <- time_points
+  return(result_list)
+}
+
+# Create lists for each dataframe
+expr3_gaba_list <- create_list_from_df(expr3_gaba)
+expr3_glut_list <- create_list_from_df(expr3_glut)
+expr4_gaba_list <- create_list_from_df(expr4_gaba)
+expr4_glut_list <- create_list_from_df(expr4_glut)
+# Convert the combined list into a named list
+# Combine all lists into one
+listInput <- list(Experiment3_P30 = expr3_glut_list$P30, 
+                  Experiment3_P60 = expr3_glut_list$P60, 
+                  Experiment3_P150 = expr3_glut_list$P150,
+                  Experiment4_P30 = expr4_glut_list$P30, 
+                  Experiment4_P60 = expr4_glut_list$P60, 
+                  Experiment4_P150 = expr4_glut_list$P150)
+upset(fromList(listInput), sets = c('Experiment3_P30', 'Experiment4_P30', 'Experiment3_P60', 'Experiment4_P60', 'Experiment3_P150', 'Experiment4_P150'), keep.order = TRUE)
+
+
+listInput <- list(Experiment3_P30 = expr3_gaba_list$P30,
+                  Experiment3_P60 = expr3_gaba_list$P60,
+                  Experiment3_P150 = expr3_gaba_list$P150,
+                  Experiment4_P30 = expr4_gaba_list$P30,
+                  Experiment4_P60 = expr4_gaba_list$P60,
+                  Experiment4_P150 = expr4_gaba_list$P150)
+
+upset(fromList(listInput), sets = c('Experiment3_P30', 'Experiment4_P30', 'Experiment3_P60', 'Experiment4_P60', 'Experiment3_P150', 'Experiment4_P150'), keep.order = TRUE)
