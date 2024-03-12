c301ed96842ed56159c1c5f5d9b63e75dc57ee9e
Osman <osharifi@ucdavis.edu>
2024-03-12 11:38:19 -0700

    mosaic bar graph

--- scripts/09_mosiacism_analysis/bargraph_Mecp2_allele.R
+++ scripts/09_mosiacism_analysis/bargraph_Mecp2_allele.R
@@ -19,4 +19,28 @@ ggplot(data_long, aes(x = ...1, y = Count, fill = Category)) +
   labs(title = "Mecp2 allele count",
        x = "Genotype",
        y = "Cell count") +
-  theme_minimal()
+  theme_minimal() +
+  guides(shape = guide_legend(override.aes = list(size = 5))) +
+  theme(legend.position = "bottom")+
+  theme_bw(base_size = 24) +
+  theme(
+    legend.position = 'right',
+    legend.background = element_rect(),
+    plot.title = element_text(angle = 0, size = 18, face = 'bold', vjust = 1),
+    plot.subtitle = element_text(angle = 0, size = 14, face = 'bold', vjust = 1),
+    plot.caption = element_text(angle = 0, size = 14, face = 'bold', vjust = 1),
+    
+    axis.text.x = element_text(angle = 90, size = 18, face = 'bold', hjust = 1.0, vjust = 0.5, colour = "black"),
+    axis.text.y = element_text(angle = 0, size = 18, face = 'bold', vjust = 0.5, colour = "black"),
+    axis.title = element_text(size = 18, face = 'bold', colour = "black"),
+    axis.title.x = element_text(size = 18, face = 'bold', colour = "black"),
+    axis.title.y = element_text(size = 18, face = 'bold', colour = "black"),
+    axis.line = element_line(colour = 'black'),
+    
+    #Legend
+    legend.key = element_blank(), # removes the border
+    legend.key.size = unit(1, "cm"), # Sets overall area/size of the legend
+    legend.text = element_text(size = 18, face = "bold"), # Text size
+    title = element_text(size = 18, face = "bold")) 
+ggsave(glue::glue("/Users/osman/Documents/GitHub/snRNA-seq-pipeline/scripts/09_mosiacism_analysis/Mecp2_allele_counts.pdf"), width = 15,
+       height = 12)
