c138a56ddb41a683dd1fe4c0ab2ec20dee48ab7e
Osman <osharifi@ucdavis.edu>
2024-05-01 14:39:56 -0700

    remove files

--- scripts/04_GO_analysis/02_make_master_godata_table.R
+++
@@ -1,8 +0,0 @@
-# Import library to run Python
-library(reticulate)
-
-# Set conda environment
-reticulate::use_condaenv("base")
-
-# Read in and run make_master_godata_table.py
-reticulate::source_python("~/GitHub/snRNA-seq-pipeline/scripts/04_GO_analysis/make_master_godata_table.py")
